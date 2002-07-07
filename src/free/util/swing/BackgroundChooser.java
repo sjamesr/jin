bdb2f13b248e7c14058377dec581c3c7ddeb2064
sasha-m <sasha-m>
2002-07-07 16:20:25 +0000

    Now uses WindowDisposingActionListener and closes when ESC is pressed.

--- src/free/util/swing/BackgroundChooser.java
+++ src/free/util/swing/BackgroundChooser.java
@@ -28,6 +28,8 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import free.util.WindowDisposingActionListener;
 
 
 /**
@@ -124,6 +126,10 @@ public class BackgroundChooser extends JDialog{
     createUI();
 
     setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+    KeyStroke closeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+    ActionListener closer = new WindowDisposingActionListener(this);
+    getRootPane().registerKeyboardAction(closer, closeKeyStroke, JComponent.WHEN_IN_FOCUSED_WINDOW);
   }
 
 
