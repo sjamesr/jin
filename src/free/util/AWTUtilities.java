200195a7c7b7ea791edebcfca06b0f1ca594cb5d
sasha-m <sasha-m>
2002-07-09 23:21:56 +0000

    Added a getFontList() method which obtains the fonts in a JDK specific manner (Toolkit.getFontList() on 1.1 and GraphicsEnvironment.getAvailableFontFamilyNames() under 1.2+)

--- src/free/util/AWTUtilities.java
+++ src/free/util/AWTUtilities.java
@@ -24,6 +24,7 @@ package free.util;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.reflect.*;
 
 
 /**
@@ -47,6 +48,36 @@ public class AWTUtilities{
 
     target.setLocation(parentLocation.x + (parentSize.width - size.width)/2, parentLocation.y + (parentSize.height - size.height)/2);
   }
+
+
+
+
+  /**
+   * Returns a list of available font names. Under JDK1.1 it uses
+   * <code>Toolkit.getFontList()</code> while under JDK1.2 (via reflection),
+   * <code>GraphicsEnvironment.getAvailableFontFamilyNames()</code>
+   */
+
+  public static String [] getAvailableFontNames(){
+    if (System.getProperty("java.version").compareTo("1.3") >= 0){
+      try{
+        // The equivalent of "return GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();"
+        Class geClass = Class.forName("java.awt.GraphicsEnvironment");
+        Method getLocalGraphicsEnvironmentMethod = geClass.getMethod("getLocalGraphicsEnvironment", null);
+        Object localGE = getLocalGraphicsEnvironmentMethod.invoke(null, new Object[0]);
+        Method getAvailableFontFamilyNamesMethod = geClass.getMethod("getAvailableFontFamilyNames", null);
+        String [] fontNames = (String [])getAvailableFontFamilyNamesMethod.invoke(localGE, new Object[0]);
+        return fontNames;
+      } catch (ClassNotFoundException e){e.printStackTrace();}
+        catch (NoSuchMethodException e){e.printStackTrace();}
+        catch (IllegalAccessException e){e.printStackTrace();}
+        catch (InvocationTargetException e){e.printStackTrace();}
+      return null;
+    }
+    else
+      return Toolkit.getDefaultToolkit().getFontList();
+  }
+
 
 
 }
