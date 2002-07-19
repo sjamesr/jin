6885f0442c8617956ede7b9549b84ab2e21b7005
sasha-m <sasha-m>
2002-07-19 22:44:10 +0000

    Renamed from  XXXBoardPiecePainter

---
+++ src/free/jin/chessclub/board/Dyche2PiecePainter.java
@@ -0,0 +1,65 @@
+/**
+ * Jin - a chess client for internet chess servers.
+ * More information is available at http://www.hightemplar.com/jin/.
+ * Copyright (C) 2002 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package free.jin.chessclub.board;
+
+import java.io.IOException;
+import free.chess.art.ResourceImagePiecePainter;
+
+/**
+ * An implementation of <code>PiecePainter</code> which draws the dyche2 piece
+ * set from blitzin. Note that the piece set belongs exlusively to
+ * chessclub.com - you may use it only as part of Jin and only with the
+ * chessclub.com server. You may *not* use it for any other purpose, 
+ * commercial or otherwise.
+ */
+
+public class Dyche2BoardPiecePainter extends ResourceImagePiecePainter{
+
+
+
+
+  /**
+   * Attempt to load the image data at classload time so that if it fails,
+   * the user of the class will know immediately and also to cache the result
+   * when the class is loaded, as is usually expected.
+   */
+
+  static{
+    try{
+      new ResourceImagePiecePainter(Dyche2BoardPiecePainter.class, "dyche2", "gif");
+    } catch (IOException e){
+        throw new RuntimeException("Unable to load the dyche2 piece set: "+e.getMessage());
+      }
+  }
+
+
+
+
+  /**
+   * Creates a new Dyche2BoardPiecePainter.
+   */
+
+  public Dyche2BoardPiecePainter() throws IOException{
+    super(Dyche2BoardPiecePainter.class, "dyche2", "gif");
+  }
+
+}
