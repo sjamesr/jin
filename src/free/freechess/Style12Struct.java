8c66a791ca28db9bd9de0f88b8b8c1ede53f3d6d
sasha-m <sasha-m>
2002-08-21 21:14:40 +0000

    Initial revision.

---
+++ src/free/freechess/Style12Struct.java
@@ -0,0 +1,600 @@
+/**
+ * Jin - a chess client for internet chess servers.
+ * More information is available at http://www.hightemplar.com/jin/.
+ * Copyright (C) 2002 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package free.freechess;
+
+import free.chess.*;
+import free.util.Struct;
+import java.util.StringTokenizer;
+
+
+/**
+ * A structure holding parsed information from a style12 line.
+ * See <A HREF="http://www.freechess.org/WWWhelp/style12.html">http://www.freechess.org/WWWhelp/style12.html</A>
+ * or the "help style12" helpfile on freechess.org for information about the format.
+ */
+
+public class Style12Struct extends Struct{
+
+
+
+  /**
+   * The constant for the user's game.
+   */
+
+  public static final int MY_GAME = 1;
+
+
+
+  /**
+   * The constant for an observed game.
+   */
+
+  public static final int OBSERVED_GAME = 2;
+
+
+
+  /**
+   * The constant for a game which is just an isolated peek into someone's game.
+   */
+
+  public static final int ISOLATED_BOARD = 3;
+
+
+
+
+  /**
+   * Creates a new Style12Struct with the specified arguments. You would usually
+   * want to use the <code>parseStyle12Line</code> method and not this
+   * constructor to obtain Style12Struct objects.
+   *
+   * @param position The current position.
+   * @param move The last move, or <code>null</code> if none.
+   * @param doublePawnPushFile The file of the double pawn push on the last
+   * move, or -1 if the last move wasn't a double pawn push.
+   * @param canWhiteCastleKingside Can white castle kingside?
+   * @param canWhiteCastleQueenside Can white castle queenside?
+   * @param canBlackCastleKingside Can black castle kingside?
+   * @param canBlackCastleQueenside Can black castle queenside?
+   * @param movesSinceIrreversible The number of moves made since the last
+   * irreversible move (0 if the last move is irreversible).
+   * @param gameNumber The game number.
+   * @param whiteName The white player's nickname.
+   * @param blackName The black player's nickname.
+   * @param gameType The code for the type of the game. Possible values are
+   * <code>MY_GAME</code>, <code>OBSERVED_GAME</code> and
+   * <code>ISOLATED_BOARD</code>.
+   * @param isPlayedGame <code>true</code> if the game is played,
+   * @param isMyTurn Is it my turn or the opponent's? Only relevant if
+   * <code>gameType</code> is <code>MY_GAME</code> and it's a played game.
+   * <code>false</code> if it's an examined game.
+   * @param initTime Initial time (in seconds) of the match.
+   * @param increment Increment (in seconds) of the match.
+   * @param whiteMaterialStrength White's material strength.
+   * @param blackMaterialStrength Black's material strength.
+   * @param whiteTime White's remaining time, in seconds.
+   * @param blackTime Black's remaining time, in seconds.
+   * @param nextMoveNumber The number of the move about to be made (standard
+   * chess numbering -- White's and Black's first moves are both 1, etc.)
+   * @param moveVerbose A verbose representation of the last move,
+   * <code>null</code> if none.
+   * @param movePretty A pretty (algebraic) representation of the last move,
+   * <code>null</code> if none.
+   * @param moveTime The amount of time taken to make the last move, in seconds.
+   * @param isBoardFlipped <code>true</code> if the board should be flipped
+   * (black at bottom), <code>false</code> otherwise.
+   */
+
+  public Style12Struct(String positionLexigraphic, String currentPlayer,
+      int doublePawnPushFile, boolean canWhiteCastleKingside, 
+      boolean canWhiteCastleQueenside, boolean canBlackCastleKingside,
+      boolean canBlackCastleQueenside, int movesSinceIrreversible,
+      int gameNumber, String whiteName, String blackName, int gameType,
+      boolean isPlayedGame, boolean isMyTurn, int initTime, int increment,
+      int whiteMaterialStrength, int blackMaterialStrength, int whiteTime,
+      int blackTime, int nextMoveNumber, String moveVerbose, String movePretty,
+      int moveTime, boolean isBoardFlipped){
+
+    if ((doublePawnPushFile < -1) || (doublePawnPushFile > 7))
+      throw new IllegalArgumentException("Bad value for double pawn push file: "+doublePawnPushFile);
+
+    if (positionLexigraphic == null)
+      throw new IllegalArgumentException("Position may not be null");
+
+    if (positionLexigraphic.length() != 64)
+      throw new IllegalArgumentException("Position string length ("+positionLexigraphic.length()+") must be 64 characters");
+
+    if ((currentPlayer == null) || ((!currentPlayer.equals("W")) && (!currentPlayer.equals("B"))))
+      throw new IllegalArgumentException("Current player string ("+currentPlayer+") must be either \"W\" or \"B\"");
+
+    if (movesSinceIrreversible < 0)
+      throw new IllegalArgumentException("Moves since irreversible move ("+movesSinceIrreversible+") cannot be negative");
+
+    if (whiteName == null)
+      throw new IllegalArgumentException("White name may not be null");
+
+    if (blackName == null)
+      throw new IllegalArgumentException("Black name may not be null");
+
+    if (initTime <= 0)
+      throw new IllegalArgumentException("Initial time ("+initTime+") must be positive");
+
+    if (increment < 0)
+      throw new IllegalArgumentException("Increment ("+increment+") may not be negative");
+
+    if (whiteMaterialStrength < 0)
+      throw new IllegalArgumentException("White's material strength ("+whiteMaterialStrength+") may not be negative");
+
+    if (blackMaterialStrength < 0)
+      throw new IllegalArgumentException("Black's material strength ("+blackMaterialStrength+") may not be negative");
+
+    if (nextMoveNumber <= 0)
+      throw new IllegalArgumentException("The next move number ("+nextMoveNumber+") must be positive");
+
+    if (moveTime < 0)
+      throw new IllegalArgumentException("The amount of time taken by the last move ("+moveTime+") may not be negative");
+
+    switch(gameType){
+      case MY_GAME:
+      case OBSERVED_GAME:
+      case ISOLATED_BOARD:
+        break;
+      default:
+        throw new IllegalArgumentException("Unknown game type: "+gameType);
+    }
+
+    setStringProperty("PositionLexigraphic", positionLexigraphic);
+    setStringProperty("CurrentPlayer", currentPlayer);
+    setIntegerProperty("DoublePawnPushFile", doublePawnPushFile);
+    setBooleanProperty("CanWhiteCastleKingside", canWhiteCastleKingside);
+    setBooleanProperty("CanWhiteCastleQueenside", canWhiteCastleQueenside);
+    setBooleanProperty("CanBlackCastleKingside", canBlackCastleKingside);
+    setBooleanProperty("CanBlackCastleQueenside", canBlackCastleQueenside);
+    setIntegerProperty("MovesSinceIrreversible", movesSinceIrreversible);
+    setIntegerProperty("GameNumber", gameNumber);
+    setStringProperty("WhiteName", whiteName);
+    setStringProperty("BlackName", blackName);
+    setIntegerProperty("GameType", gameType);
+    setBooleanProperty("IsPlayedGame", isPlayedGame);
+    setBooleanProperty("IsMyTurn", isMyTurn);
+    setIntegerProperty("InitTime", initTime);
+    setIntegerProperty("Increment", increment);
+    setIntegerProperty("WhiteMaterialStrength", whiteMaterialStrength);
+    setIntegerProperty("BlackMaterialStrength", blackMaterialStrength);
+    setIntegerProperty("WhiteTime", whiteTime);
+    setIntegerProperty("BlackTime", blackTime);
+    setIntegerProperty("NextMoveNumber", nextMoveNumber);
+    setStringProperty("MoveVerbose", moveVerbose);
+    setStringProperty("MovePretty", movePretty);
+    setIntegerProperty("MoveTime", moveTime);
+    setBooleanProperty("IsBoardFlipped", isBoardFlipped);
+  }
+
+
+
+
+  /**
+   * Parses a style12 line and returns a corresponding Style12Struct object.
+   */
+
+  public static Style12Struct parseStyle12Line(String line){
+    StringTokenizer tokens = new StringTokenizer(line, " ");
+
+    if (!tokens.nextToken().equals("<12>")) // Skip the "<12>" identifier
+      throw new IllegalArgumentException("Missing \"<12>\" identifier");
+
+    StringBuffer posBuf = new StringBuffer(64);
+    for (int i = 0; i < 8; i++)
+      posBuf.append(tokens.nextToken()); // The board
+    String positionLexigraphic = posBuf.toString();
+
+    String currentPlayer = tokens.nextToken(); // The color string, either "B" or "W"
+
+    int doublePawnPushFile = Integer.parseInt(tokens.nextToken()); // The double pawn push file
+
+    boolean canWhiteCastleKingside = parseBoolean(tokens.nextToken()); // Can white castle kingside
+    boolean canWhiteCastleQueenside = parseBoolean(tokens.nextToken()); // Can white castle queenside
+    boolean canBlackCastleKingside = parseBoolean(tokens.nextToken()); // Can black castle kingside
+    boolean canBlackCastleQueenside = parseBoolean(tokens.nextToken()); // Can black castle queenside
+
+    int movesSinceIrreversible = Integer.parseInt(tokens.nextToken()); // Number of moves since an irreversible move
+
+    int gameNumber = Integer.parseInt(tokens.nextToken()); // The game number
+
+    String whiteName = tokens.nextToken(); // White's name
+    String blackName = tokens.nextToken(); // Black's name
+
+    int myRelation = Integer.parseInt(tokens.nextToken()); // My relation to the game
+    int gameType;
+    boolean isPlayedGame;
+    switch (myRelation){
+      case -3:
+        gameType = ISOLATED_BOARD;
+        isPlayedGame = true;
+        break;
+      case -2:
+        gameType = OBSERVED_GAME;
+        isPlayedGame = false;
+        break;
+      case -1:
+      case 1:
+        gameType = MY_GAME;
+        isPlayedGame = true;
+        break;
+      case 0:
+        gameType = OBSERVED_GAME;
+        isPlayedGame = true;
+        break;
+      case 2:
+        gameType = MY_GAME;
+        isPlayedGame = false;
+        break;
+      default:
+        throw new IllegalArgumentException("Bad myRelation value: "+myRelation);
+    }
+
+    boolean isMyTurn = myRelation > 0;
+
+    int initTime = Integer.parseInt(tokens.nextToken()); // Initial time
+    int increment = Integer.parseInt(tokens.nextToken()); // Increment
+
+    int whiteMaterialStrength = Integer.parseInt(tokens.nextToken()); // White's material strength
+    int blackMaterialStrength = Integer.parseInt(tokens.nextToken()); // Black's material strength
+
+    int whiteTime = Integer.parseInt(tokens.nextToken()); // White's remaining time
+    int blackTime = Integer.parseInt(tokens.nextToken()); // Black's remaining time
+
+    int nextMoveNumber = Integer.parseInt(tokens.nextToken()); // The number of the next move
+
+    String moveVerbose = tokens.nextToken(); // The move in verbose notation
+    if (moveVerbose.equals("none"))
+      moveVerbose = null;
+
+    String moveTimeString = tokens.nextToken(); // The amount of time taken for the last move
+    moveTimeString = moveTimeString.substring(1, moveTimeString.length() - 1);
+    StringTokenizer timeTokens = new StringTokenizer(moveTimeString, ":");
+    int moveTime = 60*Integer.parseInt(timeTokens.nextToken()) + Integer.parseInt(timeTokens.nextToken());
+
+    String movePretty = tokens.nextToken(); // The move in pretty (algebraic) notation
+    if (movePretty.equals("none"))
+      movePretty = null;
+
+    boolean isBoardFlipped = parseBoolean(tokens.nextToken()); // Is the board flipped?
+
+    return new Style12Struct(positionLexigraphic, currentPlayer, doublePawnPushFile, canWhiteCastleKingside,
+      canWhiteCastleQueenside, canBlackCastleKingside, canBlackCastleQueenside, movesSinceIrreversible,
+      gameNumber, whiteName, blackName, gameType, isPlayedGame, isMyTurn, initTime, increment,
+      whiteMaterialStrength, blackMaterialStrength, whiteTime, blackTime, nextMoveNumber, moveVerbose,
+      movePretty, moveTime, isBoardFlipped);
+  }
+
+
+
+
+  /**
+   * If the given string is "1", return <code>true</code>. If it's "0", returns
+   * <code>false</code>. Otherwise throws an IllegalArgumentException.
+   */
+
+  private static boolean parseBoolean(String val){
+    if (val.equals("1"))
+      return true;
+    else if (val.equals("0"))
+      return false;
+    else
+      throw new IllegalArgumentException("Bad boolean value: "+val);
+  }
+  
+
+
+
+
+  /**
+   * Returns the current board in lexigraphic format.
+   */
+
+  public String getPositionLexigraphic(){
+    return getStringProperty("PositionLexigraphic");
+  }
+
+
+
+  
+  /**
+   * Returns a string representing the player whose turn it currently is.
+   */
+
+  public String getCurrentPlayer(){
+    return getStringProperty("CurrentPlayer");
+  }
+
+
+
+
+  /**
+   * Returns the last made move, or <code>null</code> if none.
+   */
+
+  public Move getMove(){
+    return (Move)getProperty("Move");
+  }
+
+
+
+
+  /**
+   * Returns the file of the double pawn push on the last move, or -1 if the
+   * last move wasn't a double pawn push.
+   */
+
+  public int getDoublePawnPushFile(){
+    return getIntegerProperty("DoublePawnPushFile");
+  }
+
+
+
+
+  /**
+   * Returns <code>true</code> if white can still castle kingside. Returns
+   * <code>false</code> otherwise.
+   */
+
+  public boolean canWhiteCastleKingside(){
+    return getBooleanProperty("CanWhiteCastleKingside");
+  }
+
+
+
+
+  /**
+   * Returns <code>true</code> if white can still castle queenside. Returns
+   * <code>false</code> otherwise.
+   */
+
+  public boolean canWhiteCastleQueenside(){
+    return getBooleanProperty("CanWhiteCastleQueenside");
+  }
+
+  
+
+
+  /**
+   * Returns <code>true</code> if black can still castle kingside. Returns
+   * <code>false</code> otherwise.
+   */
+
+  public boolean canBlackCastleKingside(){
+    return getBooleanProperty("CanBlackCastleKingside");
+  }
+
+
+
+
+  /**
+   * Returns <code>true</code> if black can still castle queenside. Returns
+   * <code>false</code> otherwise.
+   */
+
+  public boolean canBlackCastleQueenside(){
+    return getBooleanProperty("CanBlackCastleQueenside");
+  }
+
+
+
+
+  /**
+   * Returns the amount of moves made since the last irreversible move, or 0 if
+   * the last move was irreversible.
+   */
+
+  public int getMovesSinceIrreversible(){
+    return getIntegerProperty("MovesSinceIrreversible");
+  }
+
+
+
+
+  /**
+   * Returns the game number.
+   */
+
+  public int getGameNumber(){
+    return getIntegerProperty("GameNumber");
+  }
+
+
+
+
+  /**
+   * Returns the white player's nickname.
+   */
+
+  public String getWhiteName(){
+    return getStringProperty("WhiteName");
+  }
+
+
+
+
+  /**
+   * Returns the black player's nickname.
+   */
+
+  public String getBlackName(){
+    return getStringProperty("BlackName");
+  }
+
+
+
+
+  /**
+   * Returns the game type. Possible values are <code>MY_GAME</code>, 
+   * <code>OBSERVED_GAME</code> and <code>ISOLATED_BOARD</code>.
+   */
+
+  public int getGameType(){
+    return getIntegerProperty("GameType");
+  }
+
+
+
+
+  /**
+   * Returns <code>true</code> if the game is a played game, <code>false</code>
+   * if examined.
+   */
+
+  public boolean isPlayedGame(){
+    return getBooleanProperty("IsPlayedGame");
+  }
+
+
+
+  /**
+   * Returns <code>true</code> if it's the user's turn, <code>false</code> if
+   * it's the opponent's turn. This is only relevant if the game type is
+   * <code>MY_GAME</code> and it's a played game.
+   *
+   * @throws IllegalStateException if the game type is not <code>MY_GAME</code>
+   * or it's not a played game.
+   */
+
+  public boolean isMyTurn(){
+    if ((getGameType() != MY_GAME) || !isPlayedGame())
+      throw new IllegalStateException("Either the game type is not MY_GAME or it's not a played game.");
+
+    return getBooleanProperty("IsMyTurn");
+  }
+
+
+
+  /**
+   * Returns the initial time in the match, in seconds.
+   */
+
+  public int getInitialTime(){
+    return getIntegerProperty("InitTime");
+  }
+
+
+
+  /**
+   * Returns the increment in the time, in seconds.
+   */
+
+  public int getIncrement(){
+    return getIntegerProperty("Increment");
+  }
+
+
+
+  /**
+   * Returns white's material strength.
+   */
+
+  public int getWhiteMaterialStrength(){
+    return getIntegerProperty("WhiteMaterialStrength");
+  }
+
+
+
+  /**
+   * Returns black's material strength.
+   */
+
+  public int getBlackMaterialStrength(){
+    return getIntegerProperty("BlackMaterialStrength");
+  }
+
+
+
+  /**
+   * Returns white's remaining time, in seconds.
+   */
+
+  public int getWhiteTime(){
+    return getIntegerProperty("WhiteTime");
+  }
+
+
+
+  /**
+   * Returns black's remaining time, in seconds.
+   */
+
+  public int getBlackTime(){
+    return getIntegerProperty("BlackTime");
+  }
+
+
+
+  /**
+   * Returns the number of the next move. (standard chess numbering -- White's
+   * and Black's first moves are both 1, etc.)
+   */
+
+  public int getNextMoveNumber(){
+    return getIntegerProperty("NextMoveNumber");
+  }
+
+
+
+  /**
+   * Returns a verbose representation of the move, or <code>null</code> if none.
+   */
+
+  public String getMoveVerbose(){
+    return getStringProperty("MoveVerbose");
+  }
+
+
+
+  /**
+   * Returns a pretty (algebraic) representation of the move, or
+   * <code>null</code> if none.
+   */
+
+  public String getMovePretty(){
+    return getStringProperty("MovePretty");
+  }
+
+
+
+  /**
+   * Returns the amount of time it took to make the last move, in seconds.
+   */
+
+  public int getMoveTime(){
+    return getIntegerProperty("MoveTime");
+  }
+
+
+
+  /**
+   * Returns <code>true</code> if the board is flipped (black at bottom), false
+   * otherwise.
+   */
+
+  public boolean isBoardFlipped(){
+    return getBooleanProperty("IsBoardFlipped");
+  }
+
+}
