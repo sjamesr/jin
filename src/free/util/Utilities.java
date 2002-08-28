9b822535b4f001edef8f3600bd7e8d5280091738
sasha-m <sasha-m>
2002-08-28 22:58:01 +0000

    Initial revision.

---
+++ src/free/util/Utilities.java
@@ -0,0 +1,55 @@
+/**
+ * Jin - a chess client for internet chess servers.
+ * More information is available at http://www.hightemplar.com/jin/.
+ * Copyright (C) 2002 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package free.util;
+
+
+/**
+ * A collection of general utility methods.
+ */
+
+public class Utilities{
+
+
+
+  /**
+   * Returns <code>true</code> if the two specified objects are the same.
+   * Returns <code>false</code> otherwise. To be considered the same, the two
+   * references must either both be null or invoking <code>equals</code> on one
+   * of them with the other must return <code>true</code>.
+   */
+
+  public static boolean areEqual(Object obj1, Object obj2){
+    if (obj1 == null){
+      if (obj2 == null)
+        return true;
+      else
+        return false;
+    }
+    else if (obj2 == null)
+      return false;
+
+    return obj1.equals(obj2);
+  }
+
+
+
+}
