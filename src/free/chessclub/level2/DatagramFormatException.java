90d92efa8b6ae9e99cffbe7c4ebf2ae1024da17e
sasha-m <sasha-m>
2002-10-31 18:37:43 +0000

    Fixed "Foobar" in the license to the appropriate package name.

--- src/free/chessclub/level2/DatagramFormatException.java
+++ src/free/chessclub/level2/DatagramFormatException.java
@@ -15,8 +15,9 @@
  * General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Foobar; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with the chessclub.com connection library; if not, write to the Free
+ * Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
  package free.chessclub.level2;
