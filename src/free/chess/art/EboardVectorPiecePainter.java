3bbc29a84b0cba93b937cbec89019538f7c59fcb
sasha-m <sasha-m>
2002-11-08 13:10:40 +0000

    Fixed the copyright message.

--- src/free/chess/art/EboardVectorPiecePainter.java
+++ src/free/chess/art/EboardVectorPiecePainter.java
@@ -19,27 +19,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/**
- * Jin - a chess client for internet chess servers.
- * More information is available at http://www.hightemplar.com/jin/.
- * Copyright (C) 2002 Alexander Maryanovsky.
- * All rights reserved.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
-
 /*
   This class is based on artwork created by Felipe Paulo Guazzi Bergo and
   licensed by him under the GPL license. 
