2275c96cf8d3b642ce54c452586a064f77d0baef
sasha-m <sasha-m>
2003-02-09 11:39:46 +0000

    in the areEqual method, added a check for obj1==obj2 to speed things up.

--- src/free/util/Utilities.java
+++ src/free/util/Utilities.java
@@ -40,7 +40,7 @@ public class Utilities{
    */
 
   public static boolean areEqual(Object obj1, Object obj2){
-    return obj1 == null ? obj2 == null : obj1.equals(obj2);
+    return (obj1 == obj2) || (obj1 == null ? false : obj1.equals(obj2));
   }
 
 
