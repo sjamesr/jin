1016b3b4c404ae12b0a0d76304e41ef7370ee15b
sasha-m <sasha-m>
2003-04-11 19:41:40 +0000

    Initial revision.

---
+++ src/free/jin/ServerChoiceDialog.java
@@ -0,0 +1,150 @@
+/**
+ * Jin - a chess client for internet chess servers.
+ * More information is available at http://www.jinchess.com/.
+ * Copyright (C) 2002 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package free.jin;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.border.EmptyBorder;
+import free.util.swing.SwingUtils;
+
+
+/**
+ * The dialog that lets the user choose the server he wants to connect to.
+ */
+
+public class ServerChoiceDialog extends JDialog{
+
+
+  /**
+   * The ListModel holding the server list.
+   */
+
+  private final DefaultListModel serverListModel;
+
+
+
+  /**
+   * The selected Server.
+   */
+
+  private Server server = null;
+
+
+
+  /**
+   * Creates a new <code>ServerChoiceDialog</code> with the specified parent
+   * <code>Frame</code> and list of servers.
+   */
+
+  public ServerChoiceDialog(Frame parent, Server [] serverList){
+    super(parent, "Choose", true);
+
+    SwingUtils.registerEscapeCloser(this);
+
+    serverListModel = new DefaultListModel();
+    for (int i = 0; i < serverList.length; i++)
+      serverListModel.addElement(serverList[i]);
+
+    createUI();
+  }
+
+
+
+  /**
+   * Shows the dialog and returns the selected <code>Server</code> or
+   * <code>null</code> if the user canceled.
+   */
+
+  public Server getChoice(){
+    setVisible(true);
+
+    return server;
+  }
+
+
+
+  /**
+   * Creates the UI for this dialog.
+   */
+
+  private void createUI(){
+    final JList list = new JList(serverListModel);
+    list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+    JScrollPane scrollPane = new JScrollPane(list);
+    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+    scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    scrollPane.setPreferredSize(new Dimension(300, 80));
+
+    JLabel chooseLabel = new JLabel("Choose a server to connect to");
+    chooseLabel.setDisplayedMnemonic('C');
+    chooseLabel.setLabelFor(list);
+
+    final JButton okButton = new JButton("OK");
+    JButton cancelButton = new JButton("Cancel");
+
+    okButton.setEnabled(false);
+
+    JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    buttonPanel.add(okButton);
+    buttonPanel.add(cancelButton);
+
+    list.addListSelectionListener(new ListSelectionListener(){
+      public void valueChanged(ListSelectionEvent evt){
+        int selectedIndex = list.getSelectedIndex();
+        if (selectedIndex == -1)
+          okButton.setEnabled(false);
+        else
+          okButton.setEnabled(true);
+      }
+    });
+
+    okButton.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent evt){
+        server = (Server)list.getSelectedValue();
+        dispose();
+      }
+    });
+
+    cancelButton.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent evt){
+        server = null;
+        dispose();
+      }
+    });
+
+    JPanel content = new JPanel(new BorderLayout(5, 5));
+    content.setBorder(new EmptyBorder(5, 5, 5, 5));
+    content.add(chooseLabel, BorderLayout.NORTH);
+    content.add(scrollPane, BorderLayout.CENTER);
+    content.add(buttonPanel, BorderLayout.SOUTH);
+    setContentPane(content);
+
+    getRootPane().setDefaultButton(okButton);
+  }
+
+
+}
---
+++ src/free/util/swing/SwingUtils.java
@@ -0,0 +1,62 @@
+/**
+ * The utillib library.
+ * More information is available at http://www.jinchess.com/.
+ * Copyright (C) 2002 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * The utillib library is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * The utillib library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with utillib library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package free.util.swing;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.Window;
+import free.util.WindowDisposingActionListener;
+
+
+/**
+ * Provides various swing related utilities.
+ */
+
+public class SwingUtils{
+
+
+  /**
+   * The ESCAPE keystroke we use to close a dialog/window.
+   */
+
+  private static final KeyStroke closeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);  
+
+
+
+  /**
+   * Adds a keyboard action to the specified <code>RootPaneContainer</code> so
+   * that it is disposed when the ESCAPE key is hit. <B>Note that this only
+   * works for subclasses of <code>Window</code>.</B>
+   */
+
+  public static void registerEscapeCloser(RootPaneContainer container){
+    JRootPane rootPane = container.getRootPane();
+    Window window = SwingUtilities.windowForComponent(rootPane);
+
+    ActionListener closer = new WindowDisposingActionListener(window);
+    rootPane.registerKeyboardAction(closer, closeKeyStroke, JComponent.WHEN_IN_FOCUSED_WINDOW);
+  }
+
+
+
+}
