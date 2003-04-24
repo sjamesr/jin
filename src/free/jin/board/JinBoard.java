682ec37ed584e6eeba4021c85228b42606f613b0
sasha-m <sasha-m>
2003-04-24 13:05:21 +0000

    Initial revision.

---
+++ src/free/jin/board/JinBoard.java
@@ -0,0 +1,46 @@
+/**
+ * Jin - a chess client for internet chess servers.
+ * More information is available at http://www.jinchess.com/.
+ * Copyright (C) 2002 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package free.jin.board;
+
+import free.chess.*;
+
+
+/**
+ * An extension of <code>free.chess.JBoard</code> which implements certain chess
+ * client specific features.
+ */
+
+public class JinBoard extends JBoard{
+
+
+
+  /**
+   * Creates a new <code>JinBoard</code> with the specified initial
+   * <code>Position</code>.
+   */
+
+  public JinBoard(Position initPosition){
+    super(initPosition);
+  }
+
+
+}
