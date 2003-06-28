2675f6ddd848622f1142659195716ef625d05699
sasha-m <sasha-m>
2003-06-28 12:24:31 +0000

    Moved here from free.jin.chessclub.board.event

---
+++ src/free/jin/board/event/ArrowCircleListener.java
@@ -0,0 +1,67 @@
+/**
+ * Jin - a chess client for internet chess servers.
+ * More information is available at http://www.jinchess.com/.
+ * Copyright (C) 2002 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package free.jin.board.event;
+
+import java.util.EventListener;
+import free.jin.board.JinBoard;
+import free.chess.Square;
+
+
+/**
+ * The interface for listening to arrows and circles being added to the board
+ * or removed from it.
+ */
+
+public interface ArrowCircleListener extends EventListener{
+
+
+  /**
+   * Gets called when an arrow is added.
+   */
+
+  void arrowAdded(JinBoard board, Square fromSquare, Square toSquare);
+
+
+
+  /**
+   * Gets called when an arrow is removed.
+   */
+
+  void arrowRemoved(JinBoard board, Square fromSquare, Square toSquare);
+
+
+
+  /**
+   * Gets called when a circle is added.
+   */
+
+  void circleAdded(JinBoard board, Square circleSquare);
+
+
+
+  /**
+   * Gets called when a circle is removed.
+   */
+
+  void circleRemoved(JinBoard board, Square circleSquare);
+
+}
