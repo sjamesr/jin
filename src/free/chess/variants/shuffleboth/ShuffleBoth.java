a2737913e52bc4efa320882ac9ac3b4a8f6575ac
sasha-m <sasha-m>
2003-07-13 15:55:52 +0000

    Now extends BothSidesCastlingVariant since that's what it essentially is.

--- src/free/chess/variants/shuffleboth/ShuffleBoth.java
+++ src/free/chess/variants/shuffleboth/ShuffleBoth.java
@@ -21,6 +21,7 @@
 
 package free.chess.variants.shuffleboth;
 
+import free.chess.variants.BothSidesCastlingVariant;
 import free.chess.*;
 
 
@@ -37,7 +38,7 @@ import free.chess.*;
  * Note that castling o-o when the king is on d1/d8, moves the king to b2/b8.
  */
 
-public class ShuffleBoth extends ChesslikeGenericVariant{
+public class ShuffleBoth extends BothSidesCastlingVariant{
 
 
 
@@ -65,119 +66,6 @@ public class ShuffleBoth extends ChesslikeGenericVariant{
 
   private ShuffleBoth(){
     super(Chess.INITIAL_POSITION_FEN /* Not used anyway */, "Shuffle Both");
-  }
-
-
-
-
-  /**
-   * Returns <code>true</code> if the move defined by the given arguments is a 
-   * short castling move according to the rules of the "shuffle both" variant.
-   * Returns <code>false</code> otherwise. The result for an illegal move is
-   * undefined, but it should throw no exceptions.
-   */
-
-  public boolean isShortCastling(Position pos, Square startingSquare,
-      Square endingSquare, ChessPiece promotionTarget){
-
-    if (promotionTarget!=null)
-      return false;
-
-    // Check whether it's a "classic" castling
-    if (super.isShortCastling(pos, startingSquare, endingSquare, promotionTarget))
-      return true;
-
-    ChessPiece movingPiece = (ChessPiece)pos.getPieceAt(startingSquare);
-    ChessPiece takenPiece = (ChessPiece)pos.getPieceAt(endingSquare);
-
-    if (takenPiece!=null)
-      return false;
-
-    // Check whether it's a "reversed" castling (where o-o is castling from d1 to b1 or d8 to b8)
-    if (movingPiece==ChessPiece.WHITE_KING){
-      if (!startingSquare.equals("d1"))
-        return false;
-      else if (!endingSquare.equals("b1"))
-        return false;
-      else if (pos.getPieceAt("a1")!=ChessPiece.WHITE_ROOK)
-        return false;
-      else if (pos.getPieceAt("c1")!=null)
-        return false;
-      else
-        return true;
-    }
-    else if (movingPiece==ChessPiece.BLACK_KING){
-      if (!startingSquare.equals("d8"))
-        return false;
-      else if (!endingSquare.equals("b8"))
-        return false;
-      else if (pos.getPieceAt("a8")!=ChessPiece.BLACK_ROOK)
-        return false;
-      else if (pos.getPieceAt("c8")!=null)
-        return false;
-      else
-        return true;
-    }
-    else
-      return false;
-  }
-
-
-
-
-  /**
-   * Returns <code>true</code> if the move defined by the given arguments is a 
-   * long castling move. Returns <code>false</code> otherwise. The result for
-   * an illegal move is undefined, but it should throw no exceptions.
-   */
-
-  public boolean isLongCastling(Position pos, Square startingSquare,
-      Square endingSquare, ChessPiece promotionTarget){
-
-    if (promotionTarget!=null)
-      return false;
-
-    // Check whether it's a "classic" castling
-    if (super.isLongCastling(pos, startingSquare, endingSquare, promotionTarget))
-      return true;
-
-    ChessPiece movingPiece = (ChessPiece)pos.getPieceAt(startingSquare);
-    ChessPiece takenPiece = (ChessPiece)pos.getPieceAt(endingSquare);
-
-    if (takenPiece!=null)
-      return false;
-
-    // Check whether it's a "reversed" castling (where o-o-o is castling from d1 to f1 or d8 to f8)
-    if (movingPiece==ChessPiece.WHITE_KING){
-      if (!startingSquare.equals("d1"))
-        return false;
-      else if (!endingSquare.equals("f1"))
-        return false;
-      else if (pos.getPieceAt("h1")!=ChessPiece.WHITE_ROOK)
-        return false;
-      else if (pos.getPieceAt("g1")!=null)
-        return false;
-      else if (pos.getPieceAt("e1")!=null)
-        return false;
-      else
-        return true;
-    }
-    else if (movingPiece==ChessPiece.BLACK_KING){
-      if (!startingSquare.equals("d8"))
-        return false;
-      else if (!endingSquare.equals("f8"))
-        return false;
-      else if (pos.getPieceAt("h8")!=ChessPiece.BLACK_ROOK)
-        return false;
-      else if (pos.getPieceAt("g8")!=null)
-        return false;
-      else if (pos.getPieceAt("e8")!=null)
-        return false;
-      else
-        return true;
-    }
-    else
-      return false;
   }
 
 
@@ -287,43 +175,5 @@ public class ShuffleBoth extends ChesslikeGenericVariant{
   private static int randomInt(int max){
     return (int)(Math.random()*max);
   }
-
-
-
-
-  /**
-   * Makes the given ChessMove on the given position.
-   */
-
-  public void makeMove(Move move, Position pos, Position.Modifier modifier){
-    checkPosition(pos);
-
-    if (!(move instanceof ChessMove))
-      throw new IllegalArgumentException("The given move must be an instance of "+ChessMove.class.getName());
-
-    ChessMove cmove = (ChessMove)move;
-    Square startingSquare = cmove.getStartingSquare();
-    Square endingSquare = cmove.getEndingSquare();
-    ChessPiece movingPiece = (ChessPiece)pos.getPieceAt(startingSquare);
-
-
-    if (cmove.isCastling()&&(startingSquare.getFileChar()!='e')){ // A "reverse" castling, which we have to deal with in a special manner.
-      modifier.setPieceAt(null, startingSquare);
-      modifier.setPieceAt(movingPiece,endingSquare);
-
-      int rookStartFile = cmove.isShortCastling() ? 0 : 7;
-      int rookEndFile = cmove.isShortCastling() ? 2 : 4;
-
-      Square rookStartingSquare = Square.getInstance(rookStartFile, startingSquare.getRank());
-      Square rookEndingSquare = Square.getInstance(rookEndFile, startingSquare.getRank());
-      ChessPiece rook = (ChessPiece)pos.getPieceAt(rookStartingSquare);
-
-      modifier.setPieceAt(null, rookStartingSquare);
-      modifier.setPieceAt(rook, rookEndingSquare);
-    }
-    else
-      super.makeMove(move, pos, modifier);
-  }
-
 
 }
