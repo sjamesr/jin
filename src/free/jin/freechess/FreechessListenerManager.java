c9939d3058e3fe7b3b768c3701500f5f69ae77a3
sasha-m <sasha-m>
2003-10-29 16:02:32 +0000

    Renamed from FreechessJinListenerManager

---
+++ src/free/jin/freechess/FreechessListenerManager.java
@@ -0,0 +1,94 @@
+/**
+ * Jin - a chess client for internet chess servers.
+ * More information is available at http://www.jinchess.com/.
+ * Copyright (C) 2002 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package free.jin.freechess;
+
+import free.freechess.Ivar;
+import free.jin.event.BasicListenerManager;
+import free.jin.event.SeekListener;
+
+
+/**
+ * A freechess.org specific extension of BasicListenerManager. Used by
+ * <code>free.jin.freechess.JinFreechessConnection</code>
+ */
+
+public class FreechessListenerManager extends BasicListenerManager{
+
+
+
+  /**
+   * The source JinFreechessConnection.
+   */
+
+  private final JinFreechessConnection source;
+
+
+
+
+  /**
+   * Creates a new FreechessListenerManager with the given source
+   * <code>JinFreechessConnection</code>.
+   */
+
+  public FreechessListenerManager(JinFreechessConnection source){
+    super(source);
+
+    this.source = source;
+  }
+
+
+
+
+  /**
+   * Adds the given SeekListener to the list of listeners receiving notification
+   * of SeekEvents.
+   */
+
+  public void addSeekListener(SeekListener listener){
+    super.addSeekListener(listener);
+
+    if (listenerList.getListenerCount(SeekListener.class) == 1)
+      source.setIvarState(Ivar.SEEKINFO, true);
+    else
+      source.notFirstListenerAdded(listener);
+  }
+
+
+
+
+  /**
+   * Removes the given SeekListener from the list of listeners receiving 
+   * notification of SeekEvents.
+   */
+
+  public void removeSeekListener(SeekListener listener){
+    super.removeSeekListener(listener);
+
+    if (listenerList.getListenerCount(SeekListener.class) == 0){
+      source.setIvarState(Ivar.SEEKINFO, false);
+
+      source.lastSeekListenerRemoved();
+    }
+  }
+
+
+}
