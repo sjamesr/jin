3881361282a17e0369a6387b13850eef1d5005c9
sasha-m <sasha-m>
2004-08-05 12:04:48 +0000

    Initial revision

---
+++ resources/os-specific/applet/load_prefs.php
@@ -0,0 +1,49 @@
+<?php
+
+	$prefs_key = $_COOKIE["prefs_key"];
+	if ($prefs_key && mysql_connect("localhost", "root") && mysql_select_db("jin")){
+		
+		$result = mysql_query("SELECT PrefsBlob FROM Preferences WHERE CookieKey = '$prefs_key'");
+		$result = mysql_result($result, 0);
+		
+		/*
+			The blob is in the following format:
+			<PreferencesType>
+			<PrefLine>
+			<PrefLine>
+			..
+			<PrefLine>
+			
+			<PreferencesType>
+			<PrefLine>
+			<PrefLine>
+			..
+			<PrefLine>
+			
+			etc.
+			
+			Currently, each preference line is in the following format:
+			<PrefName>=<PrefType>;<PrefValue>
+			The allowed types and values are documented in the Java code
+			(currently in free.util.Preferences).
+		*/
+		
+		if ($result){
+			echo "<PARAM NAME=\"cookieKey\" VALUE=\"$prefs_key\">\n\t";
+			
+			$prefs = explode("\n\n", $result);
+			
+			foreach ($prefs as $typeprefsstring){
+				$typeprefs = explode("\n", $typeprefsstring);
+				
+				$type = $typeprefs[0];
+				$prefscount = count($typeprefs) - 1;
+				echo "<PARAM NAME=\"$type.prefsCount\" VALUE=\"$prefscount\">\n\t";
+				
+				for ($i = 0; $i < $prefscount; $i++)
+					printf("<PARAM NAME=\"$type.$i\" VALUE=\"%s\">\n\t", $typeprefs[$i+1]);
+			}
+		}
+	}
+
+?>
---
+++ resources/os-specific/applet/reserve_space.php
@@ -0,0 +1,98 @@
+<?php
+	if ($submit && mysql_connect("localhost", "root") && mysql_select_db("jin")){
+		
+		// Create the Preferences table if it doesn't exist yet
+		if (!mysql_query("SELECT * FROM Preferences")){
+			if (!mysql_query("CREATE TABLE Preferences ( CookieKey varchar(20), PrefsBlob blob, PRIMARY KEY (CookieKey), UNIQUE CookieKey (CookieKey) )")){
+				echo "Couldn't create Preferences table";
+				return;
+			}
+		}
+		
+		// Create the KnownUsers table if it doesn't exist yet
+		if (!mysql_query("SELECT * FROM KnownUsers")){
+			if (!mysql_query("CREATE TABLE KnownUsers ( ICSUsername varchar(30), CookieKey varchar(20), PRIMARY KEY (ICSUsername), UNIQUE ICSUsername (ICSUsername) )")){
+				echo "Couldn't create KnownUsers table";
+				return;
+			}
+		}
+		
+		
+		// Check username/password.
+		$authenticated = true;
+		
+		$username = strtolower($username);
+		
+		if ($authenticated){
+			
+			// Check whether a known user
+			$result = mysql_fetch_array(mysql_query("SELECT CookieKey FROM KnownUsers WHERE ICSUsername = '$username'"));
+			if ($result){
+				$known_user = true;
+				$prefs_key = $result[0];
+			}
+			else{
+				$known_user = false;
+				
+				// Generate a random new CookieKey
+				do{
+					$prefs_key = sprintf("%s%s%s", mt_rand(0, 10000), mt_rand(0, 10000), mt_rand(0, 10000));
+				} while (mysql_fetch_array(mysql_query("SELECT * FROM Preferences WHERE CookieKey = '$prefs_key'")));
+				
+				$insert_result = mysql_query("INSERT INTO KnownUsers VALUES ( '$username' , '$prefs_key' )");
+				$insert_result = $insert_result && mysql_query("INSERT INTO Preferences ( CookieKey ) VALUES ( '$prefs_key' )"); 
+			}
+			
+			setcookie("prefs_key", $prefs_key, time() + 60*60*24*365); // Expires in a year
+		}
+		
+	}
+		
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE> Reserve space for preferences </TITLE>
+</HEAD>
+
+<BODY>
+
+	<?php
+		if ($submit){
+			if ($authenticated){
+				if ($known_user){
+					echo "<p>You already have space reserved for your preferences. Your key is:<br>";
+					echo "<center><h2>$prefs_key</h2></center>";
+				}
+				else if ($insert_result){
+					echo "<p>Space has been reserved for your preferences on the server. Your key is:<br>";
+					echo "<center><h2>$prefs_key</h2></center>";
+				}
+				else{
+					echo "An error occurred when reserving space for your preferences";	
+				}
+			}
+			else{
+			
+			}	
+		}
+		else{
+		?>
+		
+		<P>Please provide your username and password to reserve space for preferences:
+		<P><form method="post" action="<?php echo $PHP_SELF?>">
+	
+		<table>
+			<tr><td>Username:</td><td><input type="Text" name="username"></td></tr>
+			<tr><td>Password:</td><td><input type="Password" name="password"></td></tr>
+		</table>
+		<br>
+		<input type="Submit" name="submit" value="Reserve space">
+		
+		<?php
+		}
+		?>
+</form>
+
+</BODY>
