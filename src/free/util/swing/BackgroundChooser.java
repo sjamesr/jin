61c925f3250d45c01e8e6bfe2b0e2dbeff270f1d
sasha-m <sasha-m>
2004-08-16 20:27:55 +0000

    Was completely broken with a very confusing API

--- src/free/util/swing/BackgroundChooser.java
+++ src/free/util/swing/BackgroundChooser.java
@@ -28,6 +28,8 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
 import free.util.AWTUtilities;
 import free.util.PlatformUtils;
 
@@ -35,7 +37,8 @@ import free.util.PlatformUtils;
 /**
  * A dialog which lets the user choose a background/wallpaper for a given
  * AdvancedJDesktopPane. To use a BackgroundChooser, simply instantiate
- * it and invoke <code>setVisible(true)</code> on it.
+ * it and invoke <code>setVisible(true)</code> on it. The chosen settings are
+ * applied to the specified desktop pane immediately upon selection.
  */
 
 public class BackgroundChooser extends JDialog{
@@ -49,17 +52,22 @@ public class BackgroundChooser extends JDialog{
   private final AdvancedJDesktopPane desktop;
 
 
+  
+  /**
+   * The default wallpaper color.
+   */
+   
+  private final Color defaultColor;
 
 
   /**
-   * The default wallpaper image.
+   * The default wallpaper image file.
    */
 
-  private final Image defaultImage;
+  private final File defaultImageFile;
 
 
-
-
+  
   /**
    * The default wallpaper layout style.
    */
@@ -68,50 +76,64 @@ public class BackgroundChooser extends JDialog{
 
 
 
-
   /**
-   * The file chosen for the wallpaper, or null if it's the default.
+   * The currently chosen wallpaper color.
    */
 
-  private File chosenImageFile;
-
-
-
-
-  /**
-   * The chosen background image layout style, or -1 if it's the default.
-   */
-
-  private int chosenImageLayoutStyle;
-
-
-
+  private Color color;
+  
+  
   
   /**
-   * The chosen background color, or null if it's the default.
+   * The currently chosen wallpaper image file.
+   */
+   
+  private File imageFile;
+
+
+
+  /**
+   * The chosen background image layout style.
    */
 
-  private Color chosenColor;
-
+  private int imageLayoutStyle;
 
 
 
   /**
    * Creates a new BackgroundChooser dialog which will allow selecting a
    * background/wallpaper for the given AdvancedJDesktopPane.
+   *
+   * @param parent The parent frame for the dialog.
+   * @param desktop The <code>AdvancedJDesktopPane</code> to which the choices
+   *   are applied. May be <code>null</code>, in which case the choices won't be
+   *   applied.
+   * @param defaultColor The background color used when the "Use defaults"
+   *   button is pressed. Use <code>null</code> for the color specified by the
+   *   UIManager.
+   * @param defaultImageFile The image file used when the "Use defaults" button
+   *   is pressed. Use <code>null</code> for no image.
+   * @param defaultImageLayoutStyle The image layout style used when the
+   *   "Use defaults" button is pressed.
+   * @param color The current background color of the desktop pane.
+   * @param imageFile The current image file.
+   * @param imageLayoutStyle The current image layout style.
    */
 
-  public BackgroundChooser(Frame parent, AdvancedJDesktopPane desktop, Image defaultImage,
-      int defaultImageLayoutStyle, File currentImageFile){
+  public BackgroundChooser(Frame parent, AdvancedJDesktopPane desktop,
+      Color defaultColor, File defaultImageFile, int defaultImageLayoutStyle,
+      Color color, File imageFile, int imageLayoutStyle){
     super(parent, "Pick background", true);
 
     this.desktop = desktop;
-    this.defaultImage = defaultImage;
+    
+    this.defaultColor = defaultColor;
+    this.defaultImageFile = defaultImageFile;
     this.defaultImageLayoutStyle = defaultImageLayoutStyle;
 
-    this.chosenImageFile = currentImageFile;
-    this.chosenImageLayoutStyle = desktop.getWallpaperLayoutStyle();
-    this.chosenColor = null;
+    this.color = color;
+    this.imageFile = imageFile;
+    this.imageLayoutStyle = imageLayoutStyle;
 
     createUI();
 
@@ -150,10 +172,9 @@ public class BackgroundChooser extends JDialog{
     pickImage.setDefaultCapable(false);
     useDefault.setDefaultCapable(false);
 
-    int curStyle = desktop.getWallpaperLayoutStyle();
-    final JRadioButton tileButton = new JRadioButton("Tile", curStyle == AdvancedJDesktopPane.TILE);
-    final JRadioButton scaleButton = new JRadioButton("Scale", curStyle == AdvancedJDesktopPane.SCALE);
-    final JRadioButton centerButton = new JRadioButton("Center", curStyle == AdvancedJDesktopPane.CENTER);
+    final JRadioButton tileButton = new JRadioButton("Tile", imageLayoutStyle == AdvancedJDesktopPane.TILE);
+    final JRadioButton scaleButton = new JRadioButton("Scale", imageLayoutStyle == AdvancedJDesktopPane.SCALE);
+    final JRadioButton centerButton = new JRadioButton("Center", imageLayoutStyle == AdvancedJDesktopPane.CENTER);
 
     tileButton.setMnemonic('T');
     scaleButton.setMnemonic('S');
@@ -191,15 +212,29 @@ public class BackgroundChooser extends JDialog{
         final JColorChooser colorChooser = createColorChooser();
 
         ActionListener okListener = new ActionListener(){
-
           public void actionPerformed(ActionEvent evt){
-            desktop.setBackground(colorChooser.getColor());
-            chosenColor = colorChooser.getColor();
+            Color clr = colorChooser.getColor();
+            
+            if (clr != null)
+              color = clr;
+           
+            if (color == null)
+              desktop.setBackground(UIManager.getColor("desktop"));
+            else
+              desktop.setBackground(color);
           }
-
+        };
+        
+        ActionListener cancelListener = new ActionListener(){
+          public void actionPerformed(ActionEvent evt){
+            if (color == null)
+              desktop.setBackground(UIManager.getColor("desktop"));
+            else
+              desktop.setBackground(color);
+          }
         };
 
-        JDialog dialog = JColorChooser.createDialog(BackgroundChooser.this, "Choose background color", true, colorChooser, okListener, null);
+        JDialog dialog = JColorChooser.createDialog(BackgroundChooser.this, "Choose background color", true, colorChooser, okListener, cancelListener);
         dialog.show();
       }
 
@@ -212,13 +247,16 @@ public class BackgroundChooser extends JDialog{
   
         public void actionPerformed(ActionEvent evt){
           int val = fileChooser.showOpenDialog(BackgroundChooser.this);
-          if (val == JFileChooser.APPROVE_OPTION){
-            File pic = fileChooser.getSelectedFile();
-            if ((pic != null) && pic.exists()){
-              chosenImageFile = pic;
-              desktop.setWallpaper(Toolkit.getDefaultToolkit().getImage(pic.getAbsolutePath()));
-            }
-          }
+          File pic = (val == JFileChooser.APPROVE_OPTION) ?
+            fileChooser.getSelectedFile() : null;
+            
+          if ((pic != null) && pic.exists())
+            imageFile = pic;
+          
+          if (imageFile == null)
+            desktop.setWallpaper(null);
+          else
+            desktop.setWallpaper(desktop.getToolkit().getImage(imageFile.getAbsolutePath()));
         }
   
       });
@@ -227,13 +265,27 @@ public class BackgroundChooser extends JDialog{
       useDefault.addActionListener(new ActionListener(){
   
         public void actionPerformed(ActionEvent evt){
-          chosenColor = null;
-          chosenImageFile = null;
-          chosenImageLayoutStyle = -1;
+          color = defaultColor;
+          imageFile = defaultImageFile;
+          imageLayoutStyle = defaultImageLayoutStyle;
   
-          desktop.setBackground(UIManager.getColor("desktop"));
-          desktop.setWallpaper(defaultImage);
-          desktop.setWallpaperLayoutStyle(defaultImageLayoutStyle);
+          if (color == null)
+            desktop.setBackground(UIManager.getColor("desktop"));
+          else
+            desktop.setBackground(color);
+          
+            if (imageFile == null)
+              desktop.setWallpaper(null);
+            else
+              desktop.setWallpaper(desktop.getToolkit().getImage(imageFile.getAbsolutePath()));
+            
+          desktop.setWallpaperLayoutStyle(imageLayoutStyle);
+          
+          switch(imageLayoutStyle){
+            case AdvancedJDesktopPane.CENTER: centerButton.setSelected(true); break; 
+            case AdvancedJDesktopPane.TILE: tileButton.setSelected(true); break; 
+            case AdvancedJDesktopPane.SCALE: scaleButton.setSelected(true); break; 
+          }
         }
   
       });
@@ -243,16 +295,14 @@ public class BackgroundChooser extends JDialog{
   
         public void actionPerformed(ActionEvent evt){
           Object src = evt.getSource();
-          int style = -1;
           if (src == tileButton)
-            style = AdvancedJDesktopPane.TILE;
+            imageLayoutStyle = AdvancedJDesktopPane.TILE;
           else if (src == scaleButton)
-            style = AdvancedJDesktopPane.SCALE;
+            imageLayoutStyle = AdvancedJDesktopPane.SCALE;
           else if (src == centerButton)
-            style = AdvancedJDesktopPane.CENTER;
+            imageLayoutStyle = AdvancedJDesktopPane.CENTER;
   
-          desktop.setWallpaperLayoutStyle(style);
-          chosenImageLayoutStyle = style;
+          desktop.setWallpaperLayoutStyle(imageLayoutStyle);
         }
   
       };
@@ -292,9 +342,6 @@ public class BackgroundChooser extends JDialog{
 
 
 
-
-
-
   /**
    * Creates a JColorChooser.
    */
@@ -302,25 +349,19 @@ public class BackgroundChooser extends JDialog{
   private JColorChooser createColorChooser(){
     final JColorChooser chooser = new JColorChooser(desktop.getBackground());
 
-    JPanel colorPreview = new JPanel(new FlowLayout(FlowLayout.CENTER));
-    final JPanel colorBox = new JPanel();
-    colorBox.setOpaque(true);
-    colorBox.setPreferredSize(new Dimension(100, 100));
-    colorBox.setBorder(new javax.swing.border.LineBorder(Color.black, 1));
-    colorPreview.add(colorBox);
-
-    chooser.setPreviewPanel(colorPreview);
+    chooser.setPreviewPanel(new JPanel());
     chooser.getSelectionModel().addChangeListener(new ChangeListener(){
 
       public void stateChanged(ChangeEvent evt){
-        colorBox.setBackground(chooser.getColor());
+        Color color = chooser.getColor();
+        if (color != null)
+          desktop.setBackground(color);
       }
 
     });
 
     return chooser;
   }
-
 
 
 
@@ -341,12 +382,22 @@ public class BackgroundChooser extends JDialog{
     fileChooser.setFileHidingEnabled(true);
     fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
     fileChooser.setMultiSelectionEnabled(false);
+    
+    fileChooser.addPropertyChangeListener(new PropertyChangeListener(){
+      public void propertyChange(PropertyChangeEvent evt){
+        String prop = evt.getPropertyName();
+        if (JFileChooser.SELECTED_FILE_CHANGED_PROPERTY.equals(prop)){
+          File file = (File)evt.getNewValue();
+          if (file != null){
+            Image img = desktop.getToolkit().getImage(file.getAbsolutePath());
+            desktop.setWallpaper(img);
+          }
+        }        
+      }
+    });
 
     return fileChooser;
   }
-
-
-
 
 
 
@@ -363,41 +414,34 @@ public class BackgroundChooser extends JDialog{
 
 
 
-
-
   /**
-   * Returns the chosen background color, or null if the user chose the default.
+   * Returns the chosen background color.
    */
 
-  public Color getChosenColor(){
-    return chosenColor;
+  public Color getColor(){
+    return color;
   }
 
 
 
-
-
   /**
-   * Returns the chosen background image file, or null if the user chose the
-   * default.
+   * Returns the chosen background image file.
    */
 
-  public File getChosenImageFile(){
-    return chosenImageFile;
+  public File getImageFile(){
+    return imageFile;
   }
 
 
 
-
   /**
-   * Returns the chosen background image layout style, or -1 if the user
-   * chose the default (no, this does not really make sense, it's just for
-   * consistency).
+   * Returns the chosen background image layout style.
    */
 
-  public int getChosenImageLayoutStyle(){
-    return chosenImageLayoutStyle;
+  public int getImageLayoutStyle(){
+    return imageLayoutStyle;
   }
+  
 
 
 }
