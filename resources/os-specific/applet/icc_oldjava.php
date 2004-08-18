c87de6016de915408bf72d645daccdd25a9cbfd7
sasha-m <sasha-m>
2004-08-18 00:16:44 +0000

    Renamed from .html

---
+++ resources/os-specific/applet/icc_oldjava.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE> Jin Applet </TITLE>
+</HEAD>
+
+<BODY>
+
+<CENTER>
+<APPLET CODE="free.jin.JinApplet"
+        ARCHIVE="jin.jar, libs/swingall.jar, libs/chess.jar, libs/util.jar, libs/jregex.jar, libs/timestamping.jar, servers/chessclub.jar, libs/console.jar, plugins/icc/console.jar, libs/board.jar, plugins/icc/board.jar, libs/seek.jar, plugins/icc/seek.jar, libs/sound.jar, plugins/icc/sound.jar"
+		WIDTH="400" HEIGHT="300">
+		
+	<PARAM NAME="port" VALUE="5001">
+		
+	<PARAM NAME="server.classname"
+	           VALUE="free.jin.chessclub.ChessclubServer">
+			   
+			   
+			   
+	<PARAM NAME="plugins.count" VALUE="4">
+			   
+	<PARAM NAME="plugins.0.classname"
+	           VALUE="free.jin.console.icc.ChessclubConsoleManager">
+
+	<PARAM NAME="plugins.1.classname"
+	           VALUE="free.jin.board.icc.ChessclubBoardManager">
+			   
+	<PARAM NAME="plugins.2.classname"
+	           VALUE="free.jin.seek.icc.ChessclubSoughtGraphPlugin">
+
+	<PARAM NAME="plugins.3.classname"
+	           VALUE="free.jin.sound.icc.ChessclubSoundManager">
+
+			   
+			   
+	<PARAM NAME="resources.boards" VALUE="cold-marble gray-tiles green-marble pale-wood plain red-marble slate winter wooden-dark wooden-light">
+	
+	<PARAM NAME="resources.pieces" VALUE="blitzin bookup dyche1 dyche2 dyche3 eboard xboard">
+			   
+<!--
+
+	Uncomment this to allow saving preferences.
+	
+	<PARAM NAME="savePrefsUrl" VALUE="save_prefs.php">			   
+	<PARAM NAME="reserveSpaceUrl" VALUE="reserve_space.php">			   
+			   
+	<?php
+		require "load_prefs.php";
+	?>
+	
+-->
+
+			   
+	Please enable Java to run Jin
+</APPLET>
+</CENTER>
+
+</BODY>
