c10c1422675f8bfb91a6f048bcb84d253b0ea085
sasha-m <sasha-m>
2004-09-18 23:41:42 +0000

    Added actions related parameters

--- resources/os-specific/applet/fics_oldjava.php
+++ resources/os-specific/applet/fics_oldjava.php
@@ -8,34 +8,26 @@
 
 <CENTER>
 <APPLET CODE="free.jin.JinApplet"
-        ARCHIVE="jin.jar, libs/swingall.jar, libs/chess.jar, libs/util.jar, libs/jregex.jar, libs/timesealing.jar, servers/freechess.jar, libs/console.jar, plugins/fics/console.jar, libs/board.jar, plugins/fics/board.jar, libs/seek.jar, plugins/fics/seek.jar, libs/sound.jar, plugins/fics/sound.jar"
+        ARCHIVE="jin.jar, libs/swingall.jar, libs/chess.jar, libs/util.jar, libs/jregex.jar, libs/timesealing.jar, servers/freechess.jar, libs/console.jar, plugins/fics/console.jar, libs/board.jar, plugins/fics/board.jar, libs/seek.jar, plugins/fics/seek.jar, libs/sound.jar, plugins/fics/sound.jar, plugins/actions.jar, actions/getserverhelp.jar"
 		WIDTH="400" HEIGHT="300">
 		
 	<PARAM NAME="port" value="5000">
 	
-	<PARAM NAME="server.classname"
-	           VALUE="free.jin.freechess.FreechessServer">
-			   
+	<PARAM NAME="server.classname" VALUE="free.jin.freechess.FreechessServer">
 			   
 			   
 	<PARAM NAME="plugins.count" VALUE="4">
-			   
-	<PARAM NAME="plugins.0.classname"
-	           VALUE="free.jin.console.fics.FreechessConsoleManager">
+	<PARAM NAME="plugins.0.classname" VALUE="free.jin.console.fics.FreechessConsoleManager">
+	<PARAM NAME="plugins.1.classname" VALUE="free.jin.board.fics.FreechessBoardManager">
+	<PARAM NAME="plugins.2.classname" VALUE="free.jin.seek.fics.FreechessSoughtGraphPlugin">
+	<PARAM NAME="plugins.3.classname" VALUE="free.jin.sound.fics.FreechessSoundManager">
+	<PARAM NAME="plugins.4.classname" VALUE="free.jin.actions.ActionsPlugin">
 
-	<PARAM NAME="plugins.1.classname"
-	           VALUE="free.jin.board.fics.FreechessBoardManager">
-			   
-	<PARAM NAME="plugins.2.classname"
-	           VALUE="free.jin.seek.fics.FreechessSoughtGraphPlugin">
+	<PARAM NAME="actions.count" VALUE="1">
+	<PARAM NAME="actions.0.classname" VALUE="free.jin.action.getserverhelp.GetServerHelpAction">
 
-	<PARAM NAME="plugins.3.classname"
-	           VALUE="free.jin.sound.fics.FreechessSoundManager">
-
-			   
-			   
-	<PARAM NAME="resources.boards" VALUE="cold-marble gray-tiles green-marble pale-wood plain red-marble slate winter wooden-dark">
 	
+	<PARAM NAME="resources.boards" VALUE="cold-marble gray-tiles green-marble pale-wood plain red-marble slate winter wooden-dark">
 	<PARAM NAME="resources.pieces" VALUE="fics/eboard xboard">
 			   
 <!--
--- resources/os-specific/applet/icc.php
+++ resources/os-specific/applet/icc.php
@@ -8,34 +8,26 @@
 
 <CENTER>
 <APPLET CODE="free.jin.JinApplet"
-        ARCHIVE="jin.jar, libs/chess.jar, libs/util.jar, libs/jregex.jar, libs/timestamping.jar, servers/chessclub.jar, libs/console.jar, plugins/icc/console.jar, libs/board.jar, plugins/icc/board.jar, libs/seek.jar, plugins/icc/seek.jar, libs/sound.jar, plugins/icc/sound.jar"
+        ARCHIVE="jin.jar, libs/chess.jar, libs/util.jar, libs/jregex.jar, libs/timestamping.jar, servers/chessclub.jar, libs/console.jar, plugins/icc/console.jar, libs/board.jar, plugins/icc/board.jar, libs/seek.jar, plugins/icc/seek.jar, libs/sound.jar, plugins/icc/sound.jar, plugins/actions.jar, actions/getserverhelp.jar"
 		WIDTH="400" HEIGHT="300">
 		
 	<PARAM NAME="port" VALUE="5001">
 		
-	<PARAM NAME="server.classname"
-	           VALUE="free.jin.chessclub.ChessclubServer">
-
+	<PARAM NAME="server.classname" VALUE="free.jin.chessclub.ChessclubServer">
 
 			   
-	<PARAM NAME="plugins.count" VALUE="4">
+	<PARAM NAME="plugins.count" VALUE="5">
+	<PARAM NAME="plugins.0.classname" VALUE="free.jin.console.icc.ChessclubConsoleManager">
+	<PARAM NAME="plugins.1.classname" VALUE="free.jin.board.icc.ChessclubBoardManager">
+	<PARAM NAME="plugins.2.classname" VALUE="free.jin.seek.icc.ChessclubSoughtGraphPlugin">
+	<PARAM NAME="plugins.3.classname" VALUE="free.jin.sound.icc.ChessclubSoundManager">
+	<PARAM NAME="plugins.4.classname" VALUE="free.jin.actions.ActionsPlugin">
 			   
-	<PARAM NAME="plugins.0.classname"
-	           VALUE="free.jin.console.icc.ChessclubConsoleManager">
-
-	<PARAM NAME="plugins.1.classname"
-	           VALUE="free.jin.board.icc.ChessclubBoardManager">
-			   
-	<PARAM NAME="plugins.2.classname"
-	           VALUE="free.jin.seek.icc.ChessclubSoughtGraphPlugin">
-
-	<PARAM NAME="plugins.3.classname"
-	           VALUE="free.jin.sound.icc.ChessclubSoundManager">
-
+	<PARAM NAME="actions.count" VALUE="1">
+	<PARAM NAME="actions.0.classname" VALUE="free.jin.action.getserverhelp.GetServerHelpAction">
 
 			   
 	<PARAM NAME="resources.boards" VALUE="cold-marble gray-tiles green-marble pale-wood plain red-marble slate winter wooden-dark icc/wooden-light">
-	
 	<PARAM NAME="resources.pieces" VALUE="icc/blitzin icc/bookup icc/dyche1 icc/dyche2 icc/dyche3 xboard">
 			   
 <!--
