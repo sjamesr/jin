2dbf7dfe47042daeb1d197b343a65d5f398ef76e
sasha-m <sasha-m>
2004-10-12 18:41:13 +0000

    Closed the form tag

--- resources/os-specific/applet/auth.php
+++ resources/os-specific/applet/auth.php
@@ -6,17 +6,18 @@
 
 <BODY>
 	<?php
-		if ($bad_password)
+		if (isset($bad_password) && $bad_password)
 			echo "<H3>Wrong username/password, try again.</H3><br>";
 	?>
 	<P>Type in your username and password or continue as guest:
-	<P><form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-	
-	<table>
-		<tr><td>Username:</td><td><input type="Text" name="username"></td></tr>
-		<tr><td>Password:</td><td><input type="Password" name="password"></td></tr>
-	</table>
-	<br>
-	<input type="Submit" name="continue" value="Continue">
-	<input type="Submit" name="guestContinue" value="Continue as Guest">
+	<P>
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+		<table>
+			<tr><td>Username:</td><td><input type="Text" name="username"></td></tr>
+			<tr><td>Password:</td><td><input type="Password" name="password"></td></tr>
+		</table>
+		<br>
+		<input type="Submit" name="continue" value="Continue">
+		<input type="Submit" name="guestContinue" value="Continue as Guest">
+	</form>
 </BODY>
