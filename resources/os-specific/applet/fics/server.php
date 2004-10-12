ad8af9fa6acffb06fe2af6d02e1e1a9b17450673
sasha-m <sasha-m>
2004-10-12 18:46:18 +0000

    Oops - was comparing the result of the password auth url with en empty string instead of 1

--- resources/os-specific/applet/fics/server.php
+++ resources/os-specific/applet/fics/server.php
@@ -5,7 +5,7 @@
 	// Returns the userid of the specified user or null if the specified username
 	// and password don't match.
 	function authenticate($username, $password){
-		$authenticated = (file_get_contents("http://www.freechess.org/cgi-bin/pro/Login/jin/AuthenticateUser.cgi?username=$username&password=$password") == "");
+		$authenticated = (file_get_contents("http://www.freechess.org/cgi-bin/pro/Login/jin/AuthenticateUser.cgi?username=$username&password=$password") == "1");
 		if ($authenticated)
 			return strtolower($username);
 		else
