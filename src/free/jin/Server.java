d3a12ccea55bb470335da4c89b181195fbb04fd2
sasha-m <sasha-m>
2004-12-13 11:17:57 +0000

    No longer needs JinContext as an argument in createConnection()

--- src/free/jin/Server.java
+++ src/free/jin/Server.java
@@ -48,7 +48,7 @@ public interface Server{
    * the specified connection details.
    */
 
-  Connection createConnection(JinContext context, String username, String password);
+  Connection createConnection(String username, String password);
 
 
 
