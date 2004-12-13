56671b0e55b3ee205be8db700f4caebde28ddd5b
sasha-m <sasha-m>
2004-12-13 12:29:14 +0000

    Added loadPrefsURL and savePrefsURL parameters and removed the autologin parameters. Also, removed the logout message

--- resources/os-specific/applet/fics/applet.php
+++ resources/os-specific/applet/fics/applet.php
@@ -11,25 +11,15 @@
 
 <CENTER>
 
-<?php
-	if (!$isGuest)
-		echo "<P>Welcome, $username. If you are sharing this computer, please close your browser
-or <A HREF=\"logout.php\">log out</A> after using JinApplet. You also need to do that if you want to use
-a different username.";
-?>
-
 <P>
 <APPLET CODE="free.jin.JinApplet"
         ARCHIVE="jin.jar, <?php if ($_REQUEST['oldjava']) echo "libs/swingall.jar," ?>libs/chess.jar, libs/util.jar, libs/jregex.jar, libs/fics/timesealing.jar, servers/freechess.jar, libs/console.jar, plugins/fics/console.jar, libs/board.jar, plugins/fics/board.jar, libs/seek.jar, plugins/fics/seek.jar, libs/sound.jar, plugins/fics/sound.jar, plugins/actions.jar, actions/getserverhelp.jar, actions/askquestion.jar, actions/seek.jar"
 		WIDTH="400" HEIGHT="300">
 		
-	<PARAM NAME="autologin" VALUE="true">
-	<PARAM NAME="isGuest" VALUE="<?php echo ($isGuest ? "true" : "false"); ?>">
-	<PARAM NAME="username" VALUE="<?php echo $username; ?>">
-	<PARAM NAME="password" VALUE="<?php echo $password; ?>">
+	<PARAM NAME="loadPrefsURL" VALUE="prefs.php?loadPrefs">
+	<PARAM NAME="savePrefsURL" VALUE="prefs.php?savePrefs">
 		
-		
-	<PARAM NAME="port" value="5000">
+	<PARAM NAME="port" VALUE="5000">
 		
 	<PARAM NAME="server.classname" VALUE="free.jin.freechess.FreechessServer">
 			   
@@ -50,14 +40,6 @@ a different username.";
 	<PARAM NAME="resources.boards" VALUE="cold-marble gray-tiles green-marble pale-wood plain red-marble slate winter wooden-dark">
 	<PARAM NAME="resources.pieces" VALUE="fics/eboard xboard">
 	
-	
-	<?php
-		if (!$isGuest){
-			echo "<PARAM NAME=\"savePrefsUrl\" VALUE=\"?savePrefs\">\n\t";
-			echo "<PARAM NAME=\"prefsSaveKey\" VALUE=\"" . genPrefsSaveKey($userid) . "\">\n\t";
-			echo loadPrefs($userid);
-		}
-	?>
 </APPLET>
 </CENTER>
 
--- resources/os-specific/applet/icc/applet.php
+++ resources/os-specific/applet/icc/applet.php
@@ -11,23 +11,14 @@
 
 <CENTER>
 
-<?php
-	if (!$isGuest)
-		echo "<P>Welcome, $username. If you are sharing this computer, please close your browser
-or <A HREF=\"logout.php\">log out</A> after using JinApplet. You also need to do that if you want to use
-a different username.";
-?>
-
 <P>
 <APPLET CODE="free.jin.JinApplet"
         ARCHIVE="jin.jar, <?php if ($_REQUEST['oldjava']) echo "libs/swingall.jar," ?>libs/chess.jar, libs/util.jar, libs/jregex.jar, libs/icc/timestamping.jar, servers/chessclub.jar, libs/console.jar, plugins/icc/console.jar, libs/board.jar, plugins/icc/board.jar, libs/seek.jar, plugins/icc/seek.jar, libs/sound.jar, plugins/icc/sound.jar, plugins/actions.jar, actions/getserverhelp.jar, actions/askquestion.jar, actions/seek.jar"
 		WIDTH="400" HEIGHT="300">
 		
-	<PARAM NAME="autologin" VALUE="true">
-	<PARAM NAME="isGuest" VALUE="<?php echo ($isGuest ? "true" : "false"); ?>">
-	<PARAM NAME="username" VALUE="<?php echo $username; ?>">
-	<PARAM NAME="password" VALUE="<?php echo $password; ?>">
-	
+	<PARAM NAME="loadPrefsURL" VALUE="prefs.php?loadPrefs">
+	<PARAM NAME="savePrefsURL" VALUE="prefs.php?savePrefs">
+		
 	<PARAM NAME="port" VALUE="5001">
 		
 	<PARAM NAME="server.classname" VALUE="free.jin.chessclub.ChessclubServer">
@@ -49,13 +40,6 @@ a different username.";
 	<PARAM NAME="resources.boards" VALUE="cold-marble gray-tiles green-marble pale-wood plain red-marble slate winter wooden-dark icc/wooden-light">
 	<PARAM NAME="resources.pieces" VALUE="icc/blitzin icc/bookup icc/dyche1 icc/dyche2 icc/dyche3 xboard">
 			   
-	<?php
-		if (!$isGuest){
-			echo "<PARAM NAME=\"savePrefsUrl\" VALUE=\"?savePrefs\">\n\t";
-			echo "<PARAM NAME=\"prefsSaveKey\" VALUE=\"" . genPrefsSaveKey($userid) . "\">\n\t";
-			echo loadPrefs($userid);
-		}
-	?>
 </APPLET>
 </CENTER>
 
