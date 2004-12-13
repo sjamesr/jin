2c6b4effce15b0573ed078610ca7547fe5371baa
sasha-m <sasha-m>
2004-12-13 12:26:34 +0000

    Now responsible for answering to loadPrefs/savePrefs requests

--- resources/os-specific/applet/prefs.php
+++ resources/os-specific/applet/prefs.php
@@ -1,7 +1,7 @@
 <?php
 
-	// A library of various functions related to managing users' preferences.
-	
+	require_once 'server.php';
+
 	
 	// Connects to the database and creates (if needed) the various tables
 	// needed for preferences management.
@@ -19,7 +19,7 @@
 		// when saving preferences. 
 
 		if (!mysql_query("SELECT * FROM Preferences")){
-			if (!mysql_query("CREATE TABLE Preferences ( ICSUserId varchar(20) NOT NULL, PrefsBlob blob, PrefsSaveKey varchar(20), PRIMARY KEY (ICSUserId), UNIQUE ICSUserId (ICSUserId) )"))
+			if (!mysql_query("CREATE TABLE Preferences ( ICSUserId varchar(20) NOT NULL, PrefsBlob blob, PRIMARY KEY (ICSUserId), UNIQUE ICSUserId (ICSUserId) )"))
 				return "Couldn't create Preferences table - " . mysql_error();
 		}
 		
@@ -35,7 +35,7 @@
 	function initUser($userid){
 		$result = mysql_fetch_array(mysql_query("SELECT * FROM Preferences WHERE ICSUserId = '" . addslashes($userid)  . "'"));
 		if (!$result){ // A new user
-			$result = mysql_query("INSERT INTO Preferences VALUES ( '" . addslashes($userid) . "' , NULL, NULL )");
+			$result = mysql_query("INSERT INTO Preferences VALUES ( '" . addslashes($userid) . "' , NULL )");
 			if (!$result)
 				return "Couldn't insert new user - " . mysql_error();
 		}
@@ -45,100 +45,100 @@
 	
 	
 	
-	// Generates, saves and returns a PrefsSaveKey for the specified user.
-	// Prerequisites: initUser with the id passed to this function has been
-	// called.
-	function genPrefsSaveKey($userid){
-		do{
-			$prefsSaveKey = sprintf("%s%s%s%s", mt_rand(0, 10000), mt_rand(0, 10000), mt_rand(0, 10000), mt_rand(0, 10000));
-		} while (mysql_fetch_array(mysql_query("SELECT * FROM Preferences WHERE PrefsSaveKey = '" . addslashes($prefsSaveKey) . "'")));
-		
-		mysql_query("UPDATE Preferences SET PrefsSaveKey = '" . addslashes($prefsSaveKey) . "' WHERE ICSUserId = '" . addslashes($userid) . "'");
-		
-		return $prefsSaveKey;
-	}
 	
-	
-	
-	// Saves the preferences specified by the given raw http post data and
-	// clears the PrefsSaveKey for the user.
-	// Returns an empty string on success; an error message on failure.
-	// Prerequisites: initDatabase has been called and a PrefsSaveKey has been
-	// generated for the user after the last time this function has been called.
-	function savePrefs($rawData){
-		$lines = explode("\n", $rawData); 
-		$prefsSaveKey = $lines[0];
-		$doneString = $lines[count($lines) - 1];
-		
-		if ($doneString != "Done")
-			return "Upload did not complete";
-		
-		$prefsBlob = implode("\n", array_slice($lines, 1, count($lines) - 2));
-		
-		if (!mysql_query("UPDATE Preferences SET PrefsBlob = '" . addslashes($prefsBlob) . "' WHERE PrefsSaveKey = '" . addslashes($prefsSaveKey) . "'"))
-			return "Unknown PrefsSaveKey: $prefsSaveKey";
+	// Saves the user prefs specified by the given blob.
+	// Returns null on success; an error message on failure.
+	// Prerequisites: initDatabase has been called
+	function savePrefs($userid, $userPrefsBlob){
+		if (!mysql_query("UPDATE Preferences SET PrefsBlob = '" . addslashes($userPrefsBlob) . "' WHERE ICSUserId = '" . addslashes($userid) . "'"))
+			return "Unknown user: $userid";
 			
-		mysql_query("UPDATE Preferences SET PrefsSaveKey = NULL WHERE PrefsSaveKey = '" . addslashes($prefsSaveKey) . "'");
-		
-		return "";
+		return null;
 	}
 	
 	
 	
-	// Returns a string which lists the preferences of the specified user
-	// in the format of applet parameters. The string can be simply inserted
-	// into HTML between the applet tags.
+	// Fetches the user prefs blob for the specified user. Returns an array
+	// whose first element is an error string (null if no error) and whose
+	// second element is the blob (null if an error occurred, or if the
+	// user doesn't have preferences yet).
 	// Prerequisites: initUser has been called for the specified user.
 	function loadPrefs($userid){
 		$sqlresult = mysql_query("SELECT PrefsBlob FROM Preferences WHERE ICSUserId = '" . addslashes($userid) . "'");
 			
-		if ($sqlresult)
-			$sqlresult = mysql_result($sqlresult, 0);
-		
-		/*
-			The blob is in the following format:
-			<PreferencesType>
-			<PrefLine>
-			<PrefLine>
-			..
-			<PrefLine>
-			
-			<PreferencesType>
-			<PrefLine>
-			<PrefLine>
-			..
-			<PrefLine>
-			
-			etc.
-			
-			Currently, each preference line is in the following format:
-			<PrefName>=<PrefType>;<PrefValue>
-			The allowed types and values are documented in the Java code
-			(currently in free.util.Preferences).
-		*/
-		
 		if ($sqlresult){
-			$prefs = explode("\n\n", $sqlresult);
-			
-			$result = "";
-			
-			foreach ($prefs as $typeprefsstring){
-				$typeprefs = explode("\n", $typeprefsstring);
-				
-				$type = $typeprefs[0];
-				$prefscount = count($typeprefs) - 1;
-				$result = $result . "<PARAM NAME=\"$type.prefsCount\" VALUE=\"$prefscount\">\n\t";
-				
-				for ($i = 0; $i < $prefscount; $i++)
-					$result = $result . "<PARAM NAME=\"$type.$i\" VALUE=\"" . $typeprefs[$i+1] . "\">\n\t";
-			}
-			
-			return $result;
+			if (mysql_num_rows($sqlresult) > 0)
+				return array(null, mysql_result($sqlresult, 0));
+			else
+				return array(null, null);
+		}
+		else
+			return array("Could not query database for preferences of user $userid", null);
+	}
+
+
+?>
+<?php
+
+	if (isset($_REQUEST['savePrefs']) || isset($_REQUEST['loadPrefs'])){
+		if (!$HTTP_RAW_POST_DATA){
+			echo "Missing HTTP POST data";
+			return;
 		}
 		
-		return "";
+		$data = $HTTP_RAW_POST_DATA;
+		
+		$username = strtok($data, "\n");
+		$password = strtok("\n");
+		
+		if ($username === false){
+			echo "Missing username in POST data\n";
+			return;
+		}
+		else if ($password === false){
+			echo "Missing password in POST data\n";
+			return;
+		}
+		
+		$userid = authenticate($username, $password);
+		if ($userid === null){
+			echo "Wrong username or password\n";
+			return;
+		}
+		
+		$data = substr($data, strlen($username) + strlen($password) + 2);
+		
+		initDatabase();
+		initUser($userid);
+		
+		
+		if (isset($_REQUEST['savePrefs'])){
+			$endKeyword = "PREFS_UPLOAD_END";
+			if (substr($data, strlen($data) - strlen($endKeyword)) == $endKeyword){
+				$data = substr($data, 0, strlen($data) - strlen($endKeyword));
+				$result = savePrefs($userid, $data);
+				if ($result === null)
+					echo "OK";
+				else
+					echo $result;
+			}
+			else
+				echo "Upload of preferences interrupted\n";
+		}
+		else if (isset($_REQUEST['loadPrefs'])){
+			$result = loadPrefs($userid);
+			
+			if ($result[0] !== null){ // Error
+				echo $result[0] . "\n";
+				return;
+			}
+			else if ($result[1] !== null){ // We've got prefs
+				echo "OK\n";
+				echo $result[1];
+			}
+			else{ // A new user - no prefs yet
+				echo "NOPREFS\n";
+			}
+		}
 	}
-	
-
-
 ?>
