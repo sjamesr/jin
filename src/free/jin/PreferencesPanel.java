2917c655f61b5070d20d072ffc212035a7e516a0
sasha-m <sasha-m>
2005-02-03 09:42:14 +0000

    Changed package name to free.jin

--- src/free/jin/PreferencesPanel.java
+++ src/free/jin/PreferencesPanel.java
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-package free.jin.plugin;
+package free.jin;
 
 import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
