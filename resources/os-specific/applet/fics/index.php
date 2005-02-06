b5c6fb96ced730ce8f4eff01a32de3932626498d
sasha-m <sasha-m>
2005-02-06 19:01:57 +0000

    New stuff

---
+++ resources/os-specific/applet/fics/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE> Jin Applet </TITLE>
+</HEAD>
+
+<BODY>
+
+<?php
+  require("detect_java.php");
+?>
+
+</BODY>
---
+++ resources/os-specific/applet/icc/index.php
@@ -0,0 +1,17 @@
+<?php
+   $page_title = "Jin Applet";
+   require("begin.php");
+?>
+ 
+<H1>
+	<img src="/images/bullet.gif" align="absmiddle" width="12" height="12" border="1">
+	<?php echo $page_title; ?>
+</H1>
+
+<?php
+  require("detect_java.php");
+?>
+
+<?php
+  require("end.php");
+?>
