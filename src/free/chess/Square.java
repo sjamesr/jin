64f495707ae8fd07f79c9f5f579f57826fdbc0c4
sasha-m <sasha-m>
2005-02-13 21:01:40 +0000

    Square.getInstance(int, int) no longer throws the silly SquareFormatException. Instead, it throws an IllegalArgumentException

--- src/free/chess/Square.java
+++ src/free/chess/Square.java
@@ -77,17 +77,14 @@ public class Square{
    * 
    * @param file The file index [0-7].
    * @param rank The rank index [0-7].
-   *
-   * @throws SquareFormatException if either the file or rank are out of
-   * the valid range.
    */
 
-  public static Square getInstance(int file, int rank){
+  public static Square getInstance(int file, int rank) throws IllegalArgumentException{
     if ((file < 0) || (file > 7) || (rank<0) || (rank > 7))
-      throw new SquareFormatException("file and rank must be in the range [0-7] "+
-                                      "(file:" + file + " rank:" + rank + ")");
+      throw new IllegalArgumentException("file and rank must be in the range [0-7] "+
+                                        "(file:" + file + " rank:" + rank + ")");
     if (POOL[file][rank] == null)
-      POOL[file][rank] = new Square(file,rank);
+      POOL[file][rank] = new Square(file, rank);
     return POOL[file][rank];
   }
 
