3a7061d9b26d97e88cc147044154a5c5222d8de1
sasha-m <sasha-m>
2006-11-21 12:27:06 +0000

    Changed parameter names. Also, passes the minimum required version of Java to the applet as a parameter too, now.

--- resources/os-specific/applet/detect_java.php
+++ resources/os-specific/applet/detect_java.php
@@ -1,7 +1,8 @@
 <H2>If you are not redirected to the applet page, you do not have Java. Please download and install Java from <A HREF="http://www.java.com">http://www.java.com</A></H2>.
 <APPLET CODE="DetectJava" WIDTH="1" HEIGHT="1">
-	<PARAM NAME="old_java_url" VALUE="applet.php?oldjava=1">
-	<PARAM NAME="new_java_url" VALUE="applet.php?oldjava=0">
+	<PARAM NAME="minRequiredJavaVersion" VALUE="1.4">
+	<PARAM NAME="goodJavaURL" VALUE="applet.php">
+	<PARAM NAME="badJavaURL" VALUE="bad_java.php">
 	
 	Please enable Java to run Jin
 </APPLET>
