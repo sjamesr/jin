4cf7e043d370789fcba17cff98fc3d206acd9f91
sasha-m <sasha-m>
2007-03-16 16:45:34 +0000

    Updated documentation and changed the return type of getFriends() and getOnlineFriends() from String[] to Collection.

--- src/free/jin/FriendsConnection.java
+++ src/free/jin/FriendsConnection.java
@@ -21,86 +21,81 @@
 
 package free.jin;
 
+import java.util.Collection;
+
 import free.jin.event.FriendsListenerManager;
 
 
 /**
  * An extension of the <code>Connection</code> interface which adds support for
  * friend lists (known as notify lists on ICC and FICS).
+ * Note that methods which modify friends data do not necessarily cause an
+ * immediate change, but may send the appropriate request to the server, and
+ * change the local data when it responds. Methods which query the data return
+ * immediately, based on the local data.
+ * Also, note that friends data is only guaranteed to be retrieved when at least
+ * one friends listener is registered. 
  */
 
 public interface FriendsConnection extends Connection{
-
-
+  
+  
   
   /**
-   * Returns the FriendsListenerManager which allows registering and
-   * unregistering FriendsListeners.
+   * Returns the <code>FriendsListenerManager</code> which allows registering
+   * and unregistering <code>FriendsListener</code>s.
    */
-
+  
   FriendsListenerManager getFriendsListenerManager();
   
-
-
-  /**
-   * Adds the given player to the list of "friends".
-   */
-
-  void addFriend(String name);
-
-
-
-  /**
-   * Removes the given player from the list of "friends".
-   */
-
-  void removeFriend(String name);
-
-
-
-  /**
-   * Returns the current list of friends. Note that if you've recently called
-   * <code>addFriend</code> or <code>removeFriend</code> than the list may not
-   * be updated yet, because the actual list may be stored on the server, which
-   * hasn't had time to echo the change to us yet.
-   * To make sure you get the complete list, first register as a FriendsListener
-   * and then call this method.
-   */
-
-  String [] getFriends();
-
-
-
-  /**
-   * Returns <code>true</code> if the given person is a friend. Returns
-   * <code>false</code> otherwise.
-   */
-
-  boolean isFriend(String playerName);
-
-
-
-  /**
-   * Returns the current list of online friends. Note that if you've recently
-   * called <code>addFriend</code> or <code>removeFriend</code>, this list
-   * may not be updated yet, because the actual list may be stored on the
-   * server, which hasn't had time to echo the change to us yet.
-   * To make sure you get the complete list, first register as a FriendsListener
-   * and then call this method.
-   */
-
-  String [] getOnlineFriends();
-
   
-
+  
   /**
-   * Returns <code>true</code> if the given person is a friend and is online.
-   * Note that if he's only been added recently, the server might not have had
-   * yet time to echo to use that he's online.
+   * Adds the given user to the list of friends.
+   */
+  
+  void addFriend(ServerUser user);
+  
+  
+  
+  /**
+   * Removes the given user from the list of friends.
+   */
+  
+  void removeFriend(ServerUser user);
+  
+  
+  
+  /**
+   * Returns the current set of friends (a set of <code>ServerUser</code>s).
+   **/
+  
+  Collection getFriends();
+  
+  
+  
+  /**
+   * Returns whether the specified user is a friend.
+   */
+  
+  boolean isFriend(ServerUser user);
+  
+  
+  
+  /**
+   * Returns the set of online friends (a set of <code>ServerUser</code>s).
+   **/
+  
+  Collection getOnlineFriends();
+  
+  
+  
+  /**
+   * Returns whether the specified user is a friend and is online.
    */
 
-  boolean isFriendOnline(String playerName);
-
-
-
+  boolean isFriendOnline(ServerUser user);
+  
+  
+  
 }
