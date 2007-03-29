1c14a274f36df5625db31ccdcb7b7c5f248f9aee
sasha-m <sasha-m>
2007-03-29 21:40:09 +0000

    Got rid of the UI setting the game lists display type, since game lists are no longer handled by the ConsoleManager.

--- src/free/jin/console/prefs/BehaviourPrefsPanel.java
+++ src/free/jin/console/prefs/BehaviourPrefsPanel.java
@@ -21,10 +21,13 @@
 
 package free.jin.console.prefs;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 import free.jin.BadChangesException;
-import free.jin.GameListConnection;
 import free.jin.I18n;
 import free.jin.console.ConsoleManager;
 import free.jin.ui.PreferencesPanel;
@@ -50,35 +53,10 @@ public final class BehaviourPrefsPanel extends PreferencesPanel{
   
   
   /**
-   * The radio button specifying "embedded" game lists.
-   */
-  
-  private final JRadioButton embeddedGameLists;
-  
-  
-  
-  /**
-   * The radio button specifying "external" game lists.
-   */
-  
-  private final JRadioButton externalGameLists;
-  
-  
-  
-  /**
    * The checkbox specifying copy-on-select policy.
    */
   
   private final JCheckBox copyOnSelect;
-  
-  
-  
-  /**
-   * The radio button specifying that game lists not be displayed in any special
-   * manner.
-   */
-  
-  private final JRadioButton textGameLists;
   
   
   
@@ -90,29 +68,6 @@ public final class BehaviourPrefsPanel extends PreferencesPanel{
     this.consoleManager = consoleManager;
     
     I18n i18n = I18n.get(BehaviourPrefsPanel.class);
-    
-    if (consoleManager.getConn() instanceof GameListConnection){
-      embeddedGameLists = i18n.createRadioButton("embeddedGameListsRadioButton");
-      externalGameLists = i18n.createRadioButton("externalGameListsRadioButton");
-      textGameLists = i18n.createRadioButton("textGameListsRadioButton");
-      
-      switch (consoleManager.getGameListsDisplayStyle()){
-        case ConsoleManager.EMBEDDED_GAME_LISTS: embeddedGameLists.setSelected(true); break;
-        case ConsoleManager.EXTERNAL_GAME_LISTS: externalGameLists.setSelected(true); break;
-        case ConsoleManager.NO_GAME_LISTS: textGameLists.setSelected(true); break;
-        default:
-          throw new IllegalStateException();
-      }
-      
-      embeddedGameLists.addActionListener(proxyActionListener);
-      externalGameLists.addActionListener(proxyActionListener);
-      textGameLists.addActionListener(proxyActionListener);
-    }
-    else{
-      embeddedGameLists = null;
-      externalGameLists = null;
-      textGameLists = null;
-    }
     
     copyOnSelect = i18n.createCheckBox("copyOnSelectCheckBox");
     copyOnSelect.setSelected(consoleManager.isCopyOnSelect());
@@ -132,30 +87,6 @@ public final class BehaviourPrefsPanel extends PreferencesPanel{
     
     I18n i18n = I18n.get(BehaviourPrefsPanel.class);
 
-    if (embeddedGameLists != null){
-      ButtonGroup bg = new ButtonGroup();
-      bg.add(embeddedGameLists);
-      bg.add(externalGameLists);
-      bg.add(textGameLists);
-      
-      JPanel panel = new PreferredSizedPanel();
-      panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-      panel.setBorder(BorderFactory.createCompoundBorder(
-        i18n.createTitledBorder("gameListsDisplayPanel"),
-        BorderFactory.createEmptyBorder(0, 5, 5, 5)));
-          
-      embeddedGameLists.setAlignmentX(JComponent.LEFT_ALIGNMENT);
-      externalGameLists.setAlignmentX(JComponent.LEFT_ALIGNMENT);
-      textGameLists.setAlignmentX(JComponent.LEFT_ALIGNMENT);
-      
-      panel.add(embeddedGameLists);
-      panel.add(externalGameLists);
-      panel.add(textGameLists);
-      
-      add(panel);
-      add(Box.createVerticalStrut(10));
-    }
-
     JPanel panel = new PreferredSizedPanel();
     panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
     panel.setBorder(BorderFactory.createCompoundBorder(
@@ -174,15 +105,6 @@ public final class BehaviourPrefsPanel extends PreferencesPanel{
    */
 
   public void applyChanges() throws BadChangesException{
-    if (embeddedGameLists != null){
-      if (embeddedGameLists.isSelected())
-        consoleManager.setGameListsDisplayStyle(ConsoleManager.EMBEDDED_GAME_LISTS);
-      else if (externalGameLists.isSelected())
-        consoleManager.setGameListsDisplayStyle(ConsoleManager.EXTERNAL_GAME_LISTS);
-      else
-        consoleManager.setGameListsDisplayStyle(ConsoleManager.NO_GAME_LISTS);
-    }
-    
     consoleManager.setCopyOnSelect(copyOnSelect.isSelected());
   }
   
