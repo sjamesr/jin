77d59664548d47d3c0d9b9ab605bc98aa78ef688
sasha-m <sasha-m>
2007-04-06 13:13:54 +0000

    Instead of merely accepting/declining an event, a console designation is now responsible for displaying (or not displaying) it in the console, itself. This is needed because different consoles will have different ways to display events. A personal chat console, for example, will display simply "Handle: Message" while a system console might display "Handle tells you: Message".

--- src/free/jin/console/ConsoleDesignation.java
+++ src/free/jin/console/ConsoleDesignation.java
@@ -1,3 +1,24 @@
+/**
+ * Jin - a chess client for internet chess servers.
+ * More information is available at http://www.jinchess.com/.
+ * Copyright (C) 2007 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 package free.jin.console;
 
 import free.jin.event.JinEvent;
@@ -22,10 +43,13 @@ public interface ConsoleDesignation{
   
   
   /**
-   * Returns whether the console should display the specified Jin event.
+   * Receives the specified <code>JinEvent</code>, possibly displaying it in
+   * some manner in the specified console. Before displaying any text obtained
+   * from the event, it should be encoded in the specified encoding (
+   * <code>null</code> if shouldn't).
    */
   
-  boolean accept(JinEvent evt);
+  void receive(JinEvent evt, String encoding, Console console);
   
   
   
