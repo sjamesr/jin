622a2bf4bd18068d0176c2c7ecf030d40e8247de
sasha-m <sasha-m>
2007-04-08 12:41:20 +0000

    Initial revision.

---
+++ src/free/util/Named.java
@@ -0,0 +1,73 @@
+/**
+ * The utillib library.
+ * More information is available at http://www.jinchess.com/.
+ * Copyright (C) 2007 Alexander Maryanovsky.
+ * All rights reserved.
+ *
+ * The utillib library is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * The utillib library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with utillib library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package free.util;
+
+
+
+/**
+ * A base class for classes whose objects have a name.
+ */
+
+public abstract class Named{
+  
+  
+  
+  /**
+   * The name of the object.
+   */
+  
+  private final String name;
+  
+  
+  
+  /**
+   * Creates a new <code>Named</code> object with the specified name.
+   */
+  
+  public Named(String name){
+    this.name = name;
+  }
+  
+  
+  
+  /**
+   * Returns the object's name.
+   */
+  
+  public String getName(){
+    return name;
+  }
+  
+  
+  
+  /**
+   * Returns the object's name, or the string <code>"null"</code> if the name
+   * is <code>null</code>.
+   */
+  
+  public String toString(){
+    return String.valueOf(getName());
+  }
+  
+  
+  
+}
