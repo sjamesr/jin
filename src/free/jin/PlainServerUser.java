19e5363cf58470ebc1b3f54bedff9362001276ed
sasha-m <sasha-m>
2007-04-25 19:00:52 +0000

    equals(null) will now return false.

--- src/free/jin/PlainServerUser.java
+++ src/free/jin/PlainServerUser.java
@@ -98,6 +98,9 @@ public class PlainServerUser implements ServerUser{
    */
   
   public boolean equals(Object o){
+    if (o == null)
+      return false;
+    
     if (!o.getClass().equals(this.getClass()))
       return false;
     
