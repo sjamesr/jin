863f3543e2561846debad80d057b6d2bb5cf46d3
sasha-m <sasha-m>
2007-05-10 23:39:53 +0000

    Added an isValid(file, rank) method and a getInstanceNonStrict(file, rank) method which returns null instead of throwing an exception when invoked with invalid arguments.

--- src/free/chess/Square.java
+++ src/free/chess/Square.java
@@ -23,73 +23,100 @@ package free.chess;
 
 
 /**
- * Represents a location on a chess board. <br>
- * This class does not allow creating instances of it, instead it keeps an
- * internal pool of immutable instances that can be obtained via the
- * getInstance(). This means that two Squares are the same only if they 
- * reference the same object.
+ * Represents a location on the chess board.
  */
 
 public class Square{
-
-
+  
+  
   
   /**
    * The pool of unique immutable instances of Square.
    */
-
+  
   private static final Square [][] POOL = new Square[8][8];
-
-
-
+  
+  
+  
   /**
    * The file of the square.
    */
   
   private final int file;
-
-
-
+  
+  
+  
   /**
    * The rank of the square.
    */
-
+  
   private final int rank;
-
-
-
+  
+  
+  
   /**
    * Creates a new Square with the given coordinates.
    * @param file The file index [0-7].
    * @param rank The rank index [0-7].
    */
-
+  
   private Square(int file, int rank){
     this.file = (byte)file;
     this.rank = (byte)rank;
   }
-
-
-
-  /** 
-   * Returns an instance of Square representing the given
-   * coordinates. Valid values lie in the range [0-7].
-   * 
-   * @param file The file index [0-7].
-   * @param rank The rank index [0-7].
+  
+  
+  
+  /**
+   * Returns whether the specified file and rank constitute a valid square.
    */
-
+  
+  public static boolean isValid(int file, int rank){
+    return (file >= 0) && (file < 8) && (rank >= 0) && (rank < 8);
+  }
+  
+  
+  
+  /**
+   * Returns an instance of Square representing the given coordinates. Valid
+   * values lie in the range [0-7].
+   * 
+   * @param file The file index [0-7]
+   * @param rank The rank index [0-7]
+   * 
+   * @throws IllegalArgumentException if the arguments do not constitute a valid
+   * square.
+   */
+  
   public static Square getInstance(int file, int rank) throws IllegalArgumentException{
-    if ((file < 0) || (file > 7) || (rank<0) || (rank > 7))
+    Square result = getInstanceNonStrict(file, rank);
+    
+    if (result == null)
       throw new IllegalArgumentException("file and rank must be in the range [0-7] "+
-                                        "(file:" + file + " rank:" + rank + ")");
+          "(file:" + file + " rank:" + rank + ")");
+    else
+      return result;
+  }
+  
+  
+  
+  /**
+   * Same as {@link #getInstance(int, int)}, but instead of throwing an
+   * exception for illegal values, simply returns <code>null</code>. 
+   */
+  
+  public static synchronized Square getInstanceNonStrict(int file, int rank){
+    if (!isValid(file, rank))
+      return null;
+    
     if (POOL[file][rank] == null)
       POOL[file][rank] = new Square(file, rank);
+    
     return POOL[file][rank];
   }
-
-
-
+  
+  
+  
   /**
    * Returns an instance of Square representing the given square.
    * The square is specified by a string in the usual chess format that 
@@ -100,108 +127,108 @@ public class Square{
    * @throws IllegalArgumentException if the given string is not in the
    * expected format or the coordinates are out of the valid range.
    */
-
+  
   public static Square parseSquare(String square){
     try{
-      int file = square.charAt(0)-'a';
-      int rank = square.charAt(1)-'1';
+      int file = square.charAt(0) - 'a';
+      int rank = square.charAt(1) - '1';
       return getInstance(file,rank);
     } catch (Exception e){
-        throw new IllegalArgumentException("The given string("+square+") does not represent a valid location on a chess board");
-      }
+      throw new IllegalArgumentException("The given string("+square+") does not represent a valid location on a chess board");
+    }
   }
-
-
-
+  
+  
+  
   /**
    * Returns the file of this Square, a value in the range [0-7].
    */
-
+  
   public int getFile(){
     return file;
   }
-
-
-
+  
+  
+  
   /**
    * Returns the rank of this Square, a value in the range [0-7].
    */ 
-
+  
   public int getRank(){
     return rank;
   }
-
-
-
+  
+  
+  
   /**
    * Returns a character representing the file of this Square.
    * The returned character will be in the range ['a'-'h'].
    */
- 
+  
   public char getFileChar(){
-    return (char)('a'+getFile());
+    return (char)('a' + getFile());
   }
-
-
-
+  
+  
+  
   /**
    * Returns a character representing the rank of this Square.
    * The returned character will be in the range ['1'-'8'].
    */
-
+  
   public char getRankChar(){
-    return (char)('1'+getRank());
+    return (char)('1' + getRank());
   }
-
-
-
+  
+  
+  
   /**
    * Returns a string representation of the square as is common in chess.
    * For example, for a square with file = 4 and rank = 1 this method will
    * return "e2".
    */
-
+  
   public String toString(){
     char [] buff = new char[2];
     buff[0] = getFileChar();
     buff[1] = getRankChar();
     return new String(buff);
   }
-
-
-
+  
+  
+  
   /**
    * Returns the hashCode of this Square.
    */
-
+  
   public int hashCode(){
-    return (rank<<3)+file;
+    return (rank<<3) | file;
   }
-
-
-
+  
+  
+  
   /**
    * Returns <code>true</code> if this square is the same as the square 
    * represented by the given String.
    *
    * @see #parseSquare(String).
    */
-
+  
   public boolean equals(String s){
-    return this==parseSquare(s);
+    return this == parseSquare(s);
   }
-
-
-
+  
+  
+  
   /**
    * Returns <code>true</code> if this square is the same as the square with
    * the given file and rank.
    */
-
+  
   public boolean equals(int file, int rank){
-    return this==getInstance(file, rank);
+    return this == getInstance(file, rank);
   }
-
-
+  
+  
   
 }
