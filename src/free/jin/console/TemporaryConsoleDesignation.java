ff29a697b6ea7ff86f9b26a8c72252202659f180
sasha-m <sasha-m>
2008-01-22 21:36:14 +0000

    Initial revision.

---
+++ src/free/jin/console/TemporaryConsoleDesignation.java
@@ -0,0 +1,104 @@
+package free.jin.console;
+
+import free.jin.Connection;
+import free.jin.event.JinEvent;
+
+
+
+/**
+ * A console designation which, when added, issues a specified list of commands
+ * and displays their output, without allowing further interaction with the
+ * user.
+ * 
+ * @author Maryanovsky Alexander
+ */
+
+public class TemporaryConsoleDesignation extends AbstractConsoleDesignation{
+  
+  
+  
+  /**
+   * The number of temporary console designations in the program.
+   */
+  
+  private static int temporaryConsoleCount = 0;
+  
+  
+  
+  /**
+   * Our index among the temporary console designations in the program.
+   */
+  
+  private final int index;
+  
+  
+  
+  /**
+   * The list of commands to send.
+   */
+  
+  private final String [] commands;
+  
+  
+  
+  /**
+   * Creates a new <code>TemporaryConsoleDesignation</code> which, when added
+   * will issue the specified list of commands.
+   */
+  
+  public TemporaryConsoleDesignation(Connection connection, String name, String encoding, boolean isCloseable, String [] commands){
+    super(connection, name, encoding, isCloseable);
+    
+    this.commands = (String [])commands.clone();
+    
+    synchronized(TemporaryConsoleDesignation.class){
+      index = ++temporaryConsoleCount;
+    }
+  }
+  
+  
+  
+  /**
+   * Sends our commands to the server.
+   */
+  
+  public void setConsole(Console console){
+    super.setConsole(console);
+    
+    for (int i = 0; i < commands.length; i++)
+      connection.sendTaggedCommand(commands[i], getTag());
+  }
+  
+  
+  
+  /**
+   * Returns our tag.
+   */
+  
+  protected String getTag(){
+    return "tmp-" + index;
+  }
+  
+  
+  
+  /**
+   * Accepts only events tagged by us.
+   */
+  
+  protected boolean accept(JinEvent evt){
+    return isTaggedByUs(evt);
+  }
+  
+  
+  
+  /**
+   * Returns an empty array, as we don't support any command types.
+   */
+  
+  protected CommandType[] createCommandTypes(){
+    return new CommandType[0];
+  }
+  
+  
+  
+}
