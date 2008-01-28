8b9ef04e43590e0b177dce179c35cb7b88d3fa11
sasha-m <sasha-m>
2008-01-28 08:57:02 +0000

    Changed the "no java" wording to make it clear that the user needs to wait a bit before being redirected.

--- release/applet/detect_java.php
+++ release/applet/detect_java.php
@@ -1,4 +1,4 @@
-<H2>If you are not redirected to the applet page, you do not have Java. Please download and install Java from <A HREF="http://www.java.com">http://www.java.com</A></H2>.
+<H2>If you are not redirected to the applet page within a minute, you probably do not have Java. Please download and install Java from <A HREF="http://www.java.com">http://www.java.com</A></H2>.
 <APPLET CODE="DetectJava" WIDTH="1" HEIGHT="1">
 	<PARAM NAME="minRequiredJavaVersion" VALUE="1.4">
 	<PARAM NAME="goodJavaURL" VALUE="applet.php">
